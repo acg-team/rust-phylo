9e0f119a938492d0ba9f4a0167f0fa82e9225e06
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2023-03-17 11:24:15 +0100

    Added post order traversal generator to tree

--- src/tree.rs
+++ src/tree.rs
@@ -22,6 +22,8 @@ impl Node {
 pub(crate) struct Tree {
     pub(crate) root: isize,
     pub(crate) nodes: Vec<Node>,
+    pub(crate) postorder: Vec<usize>,
+    leaf_number: usize,
 }
 
 impl Tree {
@@ -36,6 +38,8 @@ impl Tree {
                     blen: 0.0,
                 })
                 .collect(),
+            postorder: Vec::new(),
+            leaf_number: n,
         }
     }
     pub(crate) fn add_parent(
@@ -54,6 +58,29 @@ impl Tree {
         });
         self.nodes[idx_i].add_parent(idx_new, blen_i);
         self.nodes[idx_j].add_parent(idx_new, blen_j);
+    }
+
+    pub(crate) fn create_postorder(&mut self) {
+        if self.postorder.len() == 0 {
+                let mut order = Vec::<usize>::with_capacity(self.nodes.len());
+                let mut stack = Vec::<usize>::with_capacity(self.nodes.len());
+                let mut cur_root = self.root as usize;
+                stack.push(cur_root);
+                while !stack.is_empty() {
+                    cur_root = stack.pop().unwrap();
+                    order.push(cur_root);
+                    if !self.is_leaf(cur_root) {
+                        stack.push(self.nodes[cur_root].children[0]);
+                        stack.push(self.nodes[cur_root].children[1]);
+                    }
+                }
+                order.reverse();
+                self.postorder = order;
+        }
+    }
+
+    pub(crate) fn is_leaf(&self, node_idx: usize) -> bool {
+        node_idx < self.leaf_number
     }
 }
 
@@ -76,6 +103,10 @@ pub(crate) fn build_nj_tree(mut nj_data: njmat::NJMat) -> Result<Tree> {
             .recompute_new_node_distances(i, j)
             .remove_merged_nodes(i, j);
     }
-
+    tree.create_postorder();
     Ok(tree)
+}
+
+pub(crate) fn traverse_tree(tree: &mut Tree) {
+    let order = tree.create_postorder(); // should not be mutable
 }
