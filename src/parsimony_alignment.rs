5ae0b4e0b60b586c916300250c633844bbf9606b
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2023-03-17 11:37:42 +0100

    ParsAlign traceback + fixes
    Added traceback for parsimony alignment
    Refactored storing all info per node/per site
    Fixed minor computational bugs
    Moved simple test here as members became private

--- src/parsimony_alignment.rs
+++ src/parsimony_alignment.rs
@@ -1,13 +1,38 @@
-use std::fmt;
+use bio::io::fasta::Record;
 use rand::prelude::*;
+use std::fmt;
+
+use crate::{
+    sequences::{self, SequenceType},
+    tree,
+};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub(crate) enum Direction {
     Matc,
     GapX,
     GapY,
-    Ins,
     Skip,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub(crate) struct ParsAlignSiteInfo {
+    set: u8,
+    poss_gap: bool,
+    perm_gap: bool,
+}
+
+impl ParsAlignSiteInfo {
+    pub(crate) fn new(set: u8, poss_gap: bool, perm_gap: bool) -> ParsAlignSiteInfo {
+        ParsAlignSiteInfo {
+            set,
+            poss_gap,
+            perm_gap,
+        }
+    }
+    pub(crate) fn new_leaf(set: u8) -> ParsAlignSiteInfo {
+        ParsAlignSiteInfo::new(set, false, false)
+    }
 }
 
 pub(crate) struct ScoreMatrices {
@@ -43,14 +68,14 @@ impl TracebackMatrices {
 }
 
 pub(crate) struct ParsimonyAlignmentMatrices {
-    pub(crate) rows: usize,
-    pub(crate) cols: usize,
+    rows: usize,
+    cols: usize,
     pub(crate) score: ScoreMatrices,
     pub(crate) trace: TracebackMatrices,
-    pub(crate) gap_open_cost: f32,
-    pub(crate) gap_ext_cost: f32,
-    pub(crate) match_cost: f32,
-    pub(crate) direction_picker: [&'static [Direction]; 8],
+    gap_open_cost: f32,
+    gap_ext_cost: f32,
+    match_cost: f32,
+    direction_picker: [&'static [Direction]; 8],
 }
 
 impl fmt::Display for ParsimonyAlignmentMatrices {
@@ -112,9 +137,9 @@ impl ParsimonyAlignmentMatrices {
         }
     }
 
-    fn init_x(&mut self, ins: &Vec<bool>, del: &Vec<bool>) {
+    fn init_x(&mut self, node_info: &[ParsAlignSiteInfo]) {
         for i in 1..self.rows {
-            if del[i - 1] | ins[i - 1] {
+            if node_info[i - 1].perm_gap || node_info[i - 1].poss_gap {
                 self.score.x[i][0] = self.score.x[i - 1][0];
             } else if self.score.x[i - 1][0] == 0.0 {
                 self.score.x[i][0] = self.score.x[i - 1][0] + self.gap_open_cost;
@@ -129,9 +154,9 @@ impl ParsimonyAlignmentMatrices {
         }
     }
 
-    fn init_y(&mut self, ins: &Vec<bool>, del: &Vec<bool>) {
+    fn init_y(&mut self, node_info: &[ParsAlignSiteInfo]) {
         for j in 1..self.cols {
-            if del[j - 1] | ins[j - 1] {
+            if node_info[j - 1].perm_gap || node_info[j - 1].poss_gap {
                 self.score.y[0][j] = self.score.y[0][j - 1];
             } else if self.score.y[0][j - 1] == 0.0 {
                 self.score.y[0][j] = self.score.y[0][j - 1] + self.gap_open_cost;
@@ -146,7 +171,14 @@ impl ParsimonyAlignmentMatrices {
         }
     }
 
-    fn possible_match(&self, ni: usize, nj: usize, matched: bool) -> (f32, Direction) {
+    fn possible_match(
+        &self,
+        ni: usize,
+        nj: usize,
+        left_child_info: &[ParsAlignSiteInfo],
+        right_child_info: &[ParsAlignSiteInfo],
+    ) -> (f32, Direction) {
+        let matched = left_child_info[ni].set & right_child_info[nj].set == 0;
         self.select_direction(
             self.score.m[ni][nj] + if matched { self.match_cost } else { 0.0 },
             self.score.x[ni][nj] + if matched { self.match_cost } else { 0.0 },
@@ -154,20 +186,46 @@ impl ParsimonyAlignmentMatrices {
         )
     }
 
-    fn possible_gap_y(&self, ni: usize, nj: usize, dely: bool) -> (f32, Direction) {
-        self.select_direction(
-            self.score.m[ni][nj] + if dely { 0.0 } else { self.gap_open_cost },
-            self.score.x[ni][nj] + if dely { 0.0 } else { self.gap_open_cost },
-            self.score.y[ni][nj] + if dely { 0.0 } else { self.gap_ext_cost },
-        )
+    fn possible_gap_y(
+        &self,
+        ni: usize,
+        nj: usize,
+        node_info: &[ParsAlignSiteInfo],
+    ) -> (f32, Direction) {
+        if node_info[nj].perm_gap {
+            self.select_direction(
+                self.score.m[ni][nj],
+                self.score.x[ni][nj],
+                self.score.y[ni][nj],
+            )
+        } else {
+            self.select_direction(
+                self.score.m[ni][nj] + self.gap_open_cost,
+                self.score.x[ni][nj] + self.gap_open_cost,
+                self.gap_y_score(ni, nj, node_info),
+            )
+        }
     }
 
-    fn possible_gap_x(&self, ni: usize, nj: usize, delx: bool) -> (f32, Direction) {
-        self.select_direction(
-            self.score.m[ni][nj] + if delx { 0.0 } else { self.gap_open_cost },
-            self.score.x[ni][nj] + if delx { 0.0 } else { self.gap_ext_cost },
-            self.score.y[ni][nj] + if delx { 0.0 } else { self.gap_open_cost },
-        )
+    fn possible_gap_x(
+        &self,
+        ni: usize,
+        nj: usize,
+        node_info: &[ParsAlignSiteInfo],
+    ) -> (f32, Direction) {
+        if node_info[ni].perm_gap {
+            self.select_direction(
+                self.score.m[ni][nj],
+                self.score.x[ni][nj],
+                self.score.y[ni][nj],
+            )
+        } else {
+            self.select_direction(
+                self.score.m[ni][nj] + self.gap_open_cost,
+                self.gap_x_score(ni, nj, node_info),
+                self.score.y[ni][nj] + self.gap_open_cost,
+            )
+        }
     }
 
     fn select_direction(&self, sm: f32, sx: f32, sy: f32) -> (f32, Direction) {
@@ -193,68 +251,320 @@ impl ParsimonyAlignmentMatrices {
         &self,
         i: usize,
         j: usize,
-        insx: &Vec<bool>,
-        insy: &Vec<bool>,
+        insx: &bool,
+        insy: &bool,
     ) -> (f32, f32, f32, Direction) {
-        let ni = i - if insx[i - 1] { 1 } else { 0 };
-        let nj = j - if insy[j - 1] { 1 } else { 0 };
+        let ni = i - *insx as usize;
+        let nj = j - *insy as usize;
         (
             self.score.m[ni][nj],
             self.score.x[ni][nj],
             self.score.y[ni][nj],
-            Direction::Ins,
+            if *insx && *insy {
+                Direction::Matc
+            } else if *insx {
+                Direction::GapY
+            } else {
+                Direction::GapX
+            },
         )
     }
 
-    fn align_sequences(
+    fn fill_matrices(
         &mut self,
-        setsx: &[u8],
-        setsy: &[u8],
-        insx: &Vec<bool>,
-        delx: &Vec<bool>,
-        insy: &Vec<bool>,
-        dely: &Vec<bool>,
+        left_child_info: &[ParsAlignSiteInfo],
+        right_child_info: &[ParsAlignSiteInfo],
     ) {
+        self.init_x(left_child_info);
+        self.init_y(right_child_info);
         for i in 1..self.rows {
             for j in 1..self.cols {
-                if insx[i - 1] || insy[j - 1] {
+                if left_child_info[i - 1].poss_gap || right_child_info[j - 1].poss_gap {
                     (
                         self.score.m[i][j],
                         self.score.x[i][j],
                         self.score.y[i][j],
                         self.trace.m[i][j],
-                    ) = self.insertion_in_either(i, j, &insx, &insy);
+                    ) = self.insertion_in_either(
+                        i,
+                        j,
+                        &left_child_info[i - 1].poss_gap,
+                        &right_child_info[j - 1].poss_gap,
+                    );
                     self.trace.x[i][j] = self.trace.m[i][j];
                     self.trace.y[i][j] = self.trace.m[i][j];
                 } else {
                     (self.score.m[i][j], self.trace.m[i][j]) =
-                        self.possible_match(i - 1, j - 1, (setsx[i - 1] & setsy[j - 1]) == 0);
+                        self.possible_match(i - 1, j - 1, left_child_info, right_child_info);
                     (self.score.x[i][j], self.trace.x[i][j]) =
-                        self.possible_gap_x(i - 1, j, delx[i - 1]);
+                        self.possible_gap_x(i - 1, j, left_child_info);
                     (self.score.y[i][j], self.trace.y[i][j]) =
-                        self.possible_gap_y(i, j - 1, dely[j - 1]);
+                        self.possible_gap_y(i, j - 1, right_child_info);
                 }
             }
         }
     }
+
+    fn traceback(
+        &self,
+        left_child_info: &[ParsAlignSiteInfo],
+        right_child_info: &[ParsAlignSiteInfo],
+    ) -> (Vec<ParsAlignSiteInfo>, Vec<(isize, isize)>) {
+        println!("{}", self);
+
+        let mut i = self.rows - 1;
+        let mut j = self.cols - 1;
+        let (_min_val, start_traceback) =
+            self.select_direction(self.score.m[i][j], self.score.x[i][j], self.score.y[i][j]);
+        let mut trace = match start_traceback {
+            Direction::Matc => self.trace.m[i][j],
+            Direction::GapX => self.trace.x[i][j],
+            Direction::GapY => self.trace.y[i][j],
+            Direction::Skip => self.trace.m[i][j],
+        };
+        println!("{:?}", start_traceback);
+
+        let max_alignment_length = left_child_info.len() + right_child_info.len();
+        let mut node_info = Vec::<ParsAlignSiteInfo>::with_capacity(max_alignment_length);
+        let mut alignment = Vec::<(isize, isize)>::with_capacity(max_alignment_length);
+        while i > 0 || j > 0 {
+            println!("{}, {}", i, j);
+            match trace {
+                Direction::Matc => {
+                    println!("Match");
+                    if left_child_info[i - 1].perm_gap && right_child_info[j - 1].perm_gap {
+                        alignment.push((i as isize - 1, -1));
+                        alignment.push((-1, j as isize - 1));
+
+                        node_info.push(ParsAlignSiteInfo {
+                            set: 0b10000,
+                            poss_gap: true,
+                            perm_gap: true,
+                        });
+                        node_info.push(ParsAlignSiteInfo {
+                            set: 0b10000,
+                            poss_gap: true,
+                            perm_gap: true,
+                        });
+                    } else {
+                        alignment.push((i as isize - 1, j as isize - 1));
+                        let mut set = left_child_info[i - 1].set & right_child_info[j - 1].set;
+                        if set == 0 {
+                            set = left_child_info[i - 1].set | right_child_info[j - 1].set;
+                        }
+                        node_info.push(ParsAlignSiteInfo {
+                            set: set,
+                            poss_gap: false,
+                            perm_gap: false,
+                        });
+                    }
+                    i -= 1;
+                    j -= 1;
+                    trace = self.trace.m[i][j];
+                }
+                Direction::GapX => {
+                    println!("GapX");
+                    alignment.push((i as isize - 1, -1));
+                    if left_child_info[i - 1].perm_gap || left_child_info[i - 1].poss_gap {
+                        node_info.push(ParsAlignSiteInfo::new(0b10000, true, true));
+                    } else {
+                        node_info.push(ParsAlignSiteInfo::new(
+                            left_child_info[i - 1].set,
+                            true,
+                            false,
+                        ));
+                    }
+                    i -= 1;
+                    trace = self.trace.x[i][j];
+                }
+                Direction::GapY => {
+                    println!("GapY");
+                    alignment.push((-1, j as isize - 1));
+                    if right_child_info[j - 1].perm_gap || right_child_info[j - 1].poss_gap {
+                        node_info.push(ParsAlignSiteInfo::new(0b10000, true, true));
+                    } else {
+                        node_info.push(ParsAlignSiteInfo::new(
+                            right_child_info[j - 1].set,
+                            true,
+                            false,
+                        ));
+                    }
+                    j -= 1;
+                    trace = self.trace.y[i][j];
+                }
+                Direction::Skip => {
+                    if i != 0 && j != 0 {
+                        panic!("We shouldn't be here");
+                    }
+                }
+            }
+        }
+        (node_info, alignment)
+    }
+
+    fn gap_x_score(&self, ni: usize, nj: usize, node_info: &[ParsAlignSiteInfo]) -> f32 {
+        let mut skip_index = ni;
+        while skip_index > 0
+            && node_info[skip_index - 1].perm_gap
+            && self.trace.x[skip_index][nj] == Direction::GapX
+        {
+            skip_index -= 1;
+        }
+        self.score.x[skip_index][nj] + if self.trace.x[skip_index][nj] != Direction::GapX
+            && (skip_index == 0 || node_info[skip_index - 1].perm_gap)
+        {
+            self.gap_open_cost
+        } else {
+            self.gap_ext_cost
+        }
+    }
+
+    fn gap_y_score(&self, ni: usize, nj: usize, node_info: &[ParsAlignSiteInfo]) -> f32 {
+        let mut skip_index = nj;
+        while skip_index > 0
+            && node_info[skip_index - 1].perm_gap
+            && self.trace.y[ni][skip_index] == Direction::GapY
+        {
+            skip_index -= 1;
+        }
+        if self.trace.y[ni][skip_index] != Direction::GapY
+            && (skip_index == 0 || node_info[skip_index - 1].perm_gap)
+        {
+            self.score.y[ni][skip_index] + self.gap_open_cost
+        } else {
+            self.score.y[ni][skip_index] + self.gap_ext_cost
+        }
+    }
 }
 
-pub(crate) fn pars_align(sets1: &[u8], sets2: &[u8]) -> ParsimonyAlignmentMatrices {
-    let rows = sets1.len();
-    let cols = sets2.len();
-    let d1 = vec![false; rows];
-    let i1 = vec![false; rows];
-    let d2 = vec![false; cols];
-    let i2 = vec![false; cols];
+pub(crate) fn pars_align(
+    left_child_info: &[ParsAlignSiteInfo],
+    right_child_info: &[ParsAlignSiteInfo],
+) -> (Vec<ParsAlignSiteInfo>, Vec<(isize, isize)>) {
     let a = 2.5;
     let b = 0.5;
     let c = 1.0;
 
-    let mut pars_mats = ParsimonyAlignmentMatrices::new(rows + 1, cols + 1, a, b, c);
+    let mut pars_mats = ParsimonyAlignmentMatrices::new(
+        left_child_info.len() + 1,
+        right_child_info.len() + 1,
+        a,
+        b,
+        c,
+    );
+    pars_mats.fill_matrices(left_child_info, right_child_info);
+    pars_mats.traceback(left_child_info, right_child_info)
+}
 
-    pars_mats.init_x(&i1, &d1);
-    pars_mats.init_y(&i2, &d2);
-    pars_mats.align_sequences(sets1, sets2, &i1, &d1, &i2, &d2);
-    println!("{}", pars_mats);
-    pars_mats
+pub(crate) fn pars_align_on_tree(
+    tree: &tree::Tree,
+    sequences: &[Record],
+    sequence_type: &SequenceType,
+) {
+    let num = tree.nodes.len();
+    let order = &tree.postorder;
+
+    assert_eq!(num, order.len());
+
+    let mut node_info = vec![Vec::<ParsAlignSiteInfo>::new(); num];
+    let mut alignments = vec![Vec::<(isize, isize)>::new(); num];
+
+    for &node_idx in order {
+        println!("{}", node_idx);
+        if tree.is_leaf(node_idx) {
+            let pars_sets = sequences::get_parsimony_sets(&sequences[node_idx], sequence_type);
+            node_info[node_idx] = pars_sets
+                .into_iter()
+                .map(|set| ParsAlignSiteInfo::new_leaf(set))
+                .collect();
+        } else {
+            let ch1_idx = tree.nodes[node_idx].children[0];
+            let ch2_idx = tree.nodes[node_idx].children[1];
+
+            let (info, alignment) = pars_align(&node_info[ch1_idx], &node_info[ch2_idx]);
+            node_info[node_idx] = info;
+            alignments[node_idx] = alignment;
+        }
+    }
+}
+
+#[test]
+fn pars_align_matrix_simple_test() {
+    let a = 2.5;
+    let b = 0.5;
+    let c = 1.0;
+
+    let node_info_1 = vec![
+        ParsAlignSiteInfo::new(0b0100, false, false),
+        ParsAlignSiteInfo::new(0b0100, false, false),
+    ];
+    let node_info_2 = vec![
+        ParsAlignSiteInfo::new(0b1000, false, false),
+        ParsAlignSiteInfo::new(0b0100, false, false),
+    ];
+
+    let mut pars_mats = ParsimonyAlignmentMatrices::new(3, 3, a, b, c);
+
+    pars_mats.fill_matrices(&node_info_1, &node_info_2);
+    assert_eq!(
+        pars_mats.score.m,
+        vec![
+            vec![0.0, f32::INFINITY, f32::INFINITY],
+            vec![f32::INFINITY, 1.0, 2.5],
+            vec![f32::INFINITY, 3.5, 1.0]
+        ]
+    );
+    assert_eq!(
+        pars_mats.score.x,
+        vec![
+            vec![0.0, f32::INFINITY, f32::INFINITY],
+            vec![2.5, 5.0, 5.5],
+            vec![3.0, 3.5, 5.0]
+        ]
+    );
+    assert_eq!(
+        pars_mats.score.y,
+        vec![
+            vec![0.0, 2.5, 3.0],
+            vec![f32::INFINITY, 5.0, 3.5],
+            vec![f32::INFINITY, 5.5, 6.0]
+        ]
+    );
+    let any_dir = vec![Direction::Matc, Direction::GapX, Direction::GapY];
+    assert_eq!(
+        pars_mats.trace.m[0],
+        vec![Direction::Skip, Direction::GapY, Direction::GapY]
+    );
+    assert_eq!(pars_mats.trace.m[1][0], Direction::GapX);
+    assert!(any_dir.contains(&pars_mats.trace.m[1][1]));
+    assert_eq!(pars_mats.trace.m[1][2], Direction::GapY);
+    assert_eq!(
+        pars_mats.trace.m[2],
+        vec![Direction::GapX, Direction::GapX, Direction::Matc]
+    );
+
+    assert_eq!(
+        pars_mats.trace.x[0],
+        vec![Direction::Skip, Direction::GapY, Direction::GapY]
+    );
+    assert_eq!(
+        pars_mats.trace.x[1],
+        vec![Direction::GapX, Direction::GapY, Direction::GapY]
+    );
+    assert_eq!(
+        pars_mats.trace.x[2],
+        vec![Direction::GapX, Direction::Matc, Direction::Matc]
+    );
+
+    assert_eq!(
+        pars_mats.trace.y[0],
+        vec![Direction::Skip, Direction::GapY, Direction::GapY]
+    );
+    assert_eq!(
+        pars_mats.trace.y[1],
+        vec![Direction::GapX, Direction::GapX, Direction::Matc]
+    );
+    assert_eq!(pars_mats.trace.y[2][0], Direction::GapX);
+    assert_eq!(pars_mats.trace.y[2][1], Direction::GapX);
+    assert!(any_dir.contains(&pars_mats.trace.y[2][2]));
 }
