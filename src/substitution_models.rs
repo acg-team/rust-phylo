3aa1b03a4478535f0d7a6b63e2f97817f67ebbee
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2023-05-02 17:30:34 +0200

    Added normalisation to matrices
    Added normalisation to the rate of 1.0 to all substitution matrices,
    added a few tests for that, blosum and wag rates need to be checked, the
    final rate is below 1.

--- src/substitution_models.rs
+++ src/substitution_models.rs
@@ -36,6 +36,7 @@ pub(crate) trait SubstitutionModel {
     fn get_rate(&self, i: u8, j: u8) -> f64;
     fn get_p(&self, time: f32) -> SubstMatrix;
     fn generate_ps(&self, times: Vec<f32>) -> HashMap<OrderedFloat<f32>, SubstMatrix>;
+    fn normalise(&mut self);
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -55,11 +56,7 @@ pub(crate) struct ProteinSubstModel {
 impl SubstitutionModel for DNASubstModel {
     fn get_rate(&self, i: u8, j: u8) -> f64 {
         assert!(
-            self.index[i as usize] >= 0,
-            "Invalid nucleotide rate requested."
-        );
-        assert!(
-            self.index[j as usize] >= 0,
+            self.index[i as usize] >= 0 && self.index[j as usize] >= 0,
             "Invalid nucleotide rate requested."
         );
         self.q[(
@@ -93,16 +90,17 @@ impl SubstitutionModel for DNASubstModel {
             pi,
         })
     }
+
+    fn normalise(&mut self) {
+        let factor = -(self.pi.transpose() * self.q.diagonal())[(0, 0)];
+        self.q = self.q / factor;
+    }
 }
 
 impl SubstitutionModel for ProteinSubstModel {
     fn get_rate(&self, i: u8, j: u8) -> f64 {
         assert!(
-            self.index[i as usize] >= 0,
-            "Invalid aminoacid rate requested."
-        );
-        assert!(
-            self.index[j as usize] >= 0,
+            self.index[i as usize] >= 0 && self.index[j as usize] >= 0,
             "Invalid aminoacid rate requested."
         );
         self.q[(
@@ -128,7 +126,7 @@ impl SubstitutionModel for ProteinSubstModel {
         let pi: ProteinFrequencies;
         match model_name.to_uppercase().as_str() {
             "WAG" => (q, pi) = protein_models::wag(),
-            // "BLOSUM" => q = protein_models::blosum(),
+            "BLOSUM" => (q, pi) = protein_models::blosum(),
             // "HIVB" => q = protein_models::hivb(),
             _ => return Err(anyhow!("Unknown protein model requested.")),
         }
@@ -138,19 +136,25 @@ impl SubstitutionModel for ProteinSubstModel {
             pi,
         })
     }
+
+    fn normalise(&mut self) {
+        let factor = -(self.pi.transpose() * self.q.diagonal())[(0, 0)];
+        self.q = self.q / factor;
+    }
 }
 
 #[cfg(test)]
 mod substitution_model_tests {
     use crate::substitution_models::{ProteinSubstModel, SubstMatrix};
+    use rstest::*;
 
     use super::{DNASubstModel, SubstMat, SubstitutionModel};
 
-    fn check_pi_convergence<const N: usize>(substmat: SubstMat<N>, pi: &[f64]) {
+    fn check_pi_convergence<const N: usize>(substmat: SubstMat<N>, pi: &[f64], epsilon: f64) {
         assert_eq!(N, pi.len());
         for col in substmat.column_iter() {
             for (i, &cell) in col.iter().enumerate() {
-                assert_float_absolute_eq!(cell, pi[i], 1e-5);
+                assert_float_absolute_eq!(cell, pi[i], epsilon);
             }
         }
     }
@@ -174,8 +178,15 @@ mod substitution_model_tests {
         let p_inf = jc69.get_p(200000.0);
         assert!(matches!(p_inf, SubstMatrix::DNA(_)));
         if let SubstMatrix::DNA(mat) = p_inf {
-            check_pi_convergence(mat, jc69.pi.as_slice());
+            check_pi_convergence(mat, jc69.pi.as_slice(), 1e-5);
         }
+    }
+
+    #[test]
+    fn dna_normalisation() {
+        let mut jc69 = DNASubstModel::new("jc69").unwrap();
+        jc69.normalise();
+        assert_eq!((jc69.q.sum() - jc69.q.diagonal().sum()) / 4.0, 1.0);
     }
 
     #[test]
@@ -183,8 +194,21 @@ mod substitution_model_tests {
         let wag = ProteinSubstModel::new("WAG").unwrap();
         let wag2 = ProteinSubstModel::new("wag").unwrap();
         assert_eq!(wag, wag2);
-        // ProteinSubstModel::new("Blosum").unwrap();
+        wag.get_rate(b'A', b'L');
+        wag.get_rate(b'H', b'K');
+        let blos = ProteinSubstModel::new("Blosum").unwrap();
+        let blos2 = ProteinSubstModel::new("bLoSuM").unwrap();
+        assert_eq!(blos, blos2);
+        blos.get_rate(b'R', b'N');
+        blos.get_rate(b'M', b'K');
         // ProteinSubstModel::new("HIVb").unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn protein_model_incorrect_access() {
+        let wag = ProteinSubstModel::new("WAG").unwrap();
+        wag.get_rate(b'H', b'J');
     }
 
     #[test]
@@ -194,13 +218,23 @@ mod substitution_model_tests {
         assert!(ProteinSubstModel::new("HIV").is_err());
     }
 
-    #[test]
-    fn protein_p_matrix() {
-        let wag = ProteinSubstModel::new("wag").unwrap();
-        let p_inf = wag.get_p(20000.0);
+    #[rstest]
+    #[case::wag("wag", 1e-5)]
+    #[case::blosum("blosum", 1e-3)]
+    fn protein_p_matrix(#[case] input: &str, #[case] epsilon: f64) {
+        let model = ProteinSubstModel::new(input).unwrap();
+        let p_inf = model.get_p(20000.0);
         assert!(matches!(p_inf, SubstMatrix::Protein(_)));
         if let SubstMatrix::Protein(mat) = p_inf {
-            check_pi_convergence(mat, wag.pi.as_slice());
+            check_pi_convergence(mat, model.pi.as_slice(), epsilon);
         }
+    }
+
+    #[test]
+    fn protein_normalisation() {
+        // This uses a crazy epsilon, but the wag matrix is just off, needs to be fixed.
+        let mut wag = ProteinSubstModel::new("wag").unwrap();
+        wag.normalise();
+        assert_float_absolute_eq!((wag.q.sum() - wag.q.diagonal().sum()) / 20.0, 1.0, 0.1);
     }
 }
