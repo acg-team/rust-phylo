c0fc1e0234d604870994c9b36b292ef0216e93c8
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2023-05-03 12:12:30 +0200

    Added pi to k80 and gtr
    Added stationary frequencies pi to K80 and GTR models

--- src/substitution_models/dna_models.rs
+++ src/substitution_models/dna_models.rs
@@ -21,33 +21,36 @@ pub(crate) fn jc69() -> (DNASubstMatrix, DNAFreqVector) {
     )
 }
 
-pub(crate) fn k80(alpha: f64, beta: f64) -> DNASubstMatrix {
-    DNASubstMatrix::from([
-        [
-            -1.0,
-            alpha / (alpha + 2.0 * beta),
-            beta / (alpha + 2.0 * beta),
-            beta / (alpha + 2.0 * beta),
-        ],
-        [
-            alpha / (alpha + 2.0 * beta),
-            -1.0,
-            beta / (alpha + 2.0 * beta),
-            beta / (alpha + 2.0 * beta),
-        ],
-        [
-            beta / (alpha + 2.0 * beta),
-            beta / (alpha + 2.0 * beta),
-            -1.0,
-            alpha / (alpha + 2.0 * beta),
-        ],
-        [
-            beta / (alpha + 2.0 * beta),
-            beta / (alpha + 2.0 * beta),
-            alpha / (alpha + 2.0 * beta),
-            -1.0,
-        ],
-    ])
+pub(crate) fn k80(alpha: f64, beta: f64) -> (DNASubstMatrix, DNAFreqVector) {
+    (
+        DNASubstMatrix::from([
+            [
+                -1.0,
+                alpha / (alpha + 2.0 * beta),
+                beta / (alpha + 2.0 * beta),
+                beta / (alpha + 2.0 * beta),
+            ],
+            [
+                alpha / (alpha + 2.0 * beta),
+                -1.0,
+                beta / (alpha + 2.0 * beta),
+                beta / (alpha + 2.0 * beta),
+            ],
+            [
+                beta / (alpha + 2.0 * beta),
+                beta / (alpha + 2.0 * beta),
+                -1.0,
+                alpha / (alpha + 2.0 * beta),
+            ],
+            [
+                beta / (alpha + 2.0 * beta),
+                beta / (alpha + 2.0 * beta),
+                alpha / (alpha + 2.0 * beta),
+                -1.0,
+            ],
+        ]),
+        DNAFreqVector::from(JC69_PI_ARR),
+    )
 }
 
 pub(crate) fn gtr(
@@ -61,39 +64,37 @@ pub(crate) fn gtr(
     r_ca: f64,
     r_cg: f64,
     r_ag: f64,
-) -> DNASubstMatrix {
-    let q = DNASubstMatrix::from([
-        [
-            -(r_tc * f_c + r_ta * f_a + r_tg * f_g),
-            r_tc * f_c,
-            r_ta * f_a,
-            r_tg * f_g,
-        ],
-        [
-            r_tc * f_t,
-            -(r_tc * f_t + r_ca * f_a + r_cg * f_g),
-            r_ca * f_a,
-            r_cg * f_g,
-        ],
-        [
-            r_ta * f_t,
-            r_ca * f_c,
-            -(r_ta * f_t + r_ca * f_c + r_ag * f_g),
-            r_ag * f_g,
-        ],
-        [
-            r_tg * f_t,
-            r_cg * f_c,
-            r_ag * f_a,
-            -(r_tg * f_t + r_cg * f_c + r_ag * f_a),
-        ],
-    ]);
-    let scale = 1.0
-        / (f_t * (r_tc * f_c + r_ta * f_a + r_tg * f_g)
-            + f_c * (r_tc * f_t + r_ca * f_a + r_cg * f_g)
-            + f_a * (r_ta * f_t + r_ca * f_c + r_ag * f_g)
-            + f_g * (r_tg * f_t + r_cg * f_c + r_ag * f_a));
-    q * scale
+) -> (DNASubstMatrix, DNAFreqVector) {
+    assert_f64_near!(f_t + f_c + f_a + f_g, 1.0);
+    (
+        DNASubstMatrix::from([
+            [
+                -(r_tc * f_c + r_ta * f_a + r_tg * f_g),
+                r_tc * f_c,
+                r_ta * f_a,
+                r_tg * f_g,
+            ],
+            [
+                r_tc * f_t,
+                -(r_tc * f_t + r_ca * f_a + r_cg * f_g),
+                r_ca * f_a,
+                r_cg * f_g,
+            ],
+            [
+                r_ta * f_t,
+                r_ca * f_c,
+                -(r_ta * f_t + r_ca * f_c + r_ag * f_g),
+                r_ag * f_g,
+            ],
+            [
+                r_tg * f_t,
+                r_cg * f_c,
+                r_ag * f_a,
+                -(r_tg * f_t + r_cg * f_c + r_ag * f_a),
+            ],
+        ]),
+        DNAFreqVector::from([f_t, f_c, f_a, f_g]),
+    )
 }
 
 const JC69_ARR: [[f64; 4]; 4] = [
