45d37316970e76e0164269653516a19735971481
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2023-06-08 15:52:44 +0200

    Tests for simple pars costs
    Added test cases for the simple parsimony costs. Very few test cases
    because this specific scoring scheme is also tested in parsimony_matrices.

--- src/parsimony_alignment/parsimony_costs/parsimony_costs_simple.rs
+++ src/parsimony_alignment/parsimony_costs/parsimony_costs_simple.rs
@@ -14,8 +14,8 @@ impl ParsimonyCostsSimple {
         ParsimonyCostsSimple {
             costs: BranchParsimonyCostsSimple {
                 mismatch: mismatch,
-                gap_open: gap_open,
-                gap_ext: gap_ext,
+                gap_open: gap_open * mismatch,
+                gap_ext: gap_ext * mismatch,
             },
         }
     }
@@ -47,5 +47,34 @@ impl BranchParsimonyCosts for BranchParsimonyCostsSimple {
 
     fn gap_open_cost(&self) -> f64 {
         self.gap_open
+    }
+}
+
+#[cfg(test)]
+mod parsimony_costs_simple_test {
+    use super::ParsimonyCostsSimple;
+    use crate::parsimony_alignment::parsimony_costs::BranchParsimonyCosts;
+
+    #[test]
+    fn default_costs() {
+        let default_costs = ParsimonyCostsSimple::new_default();
+        assert_eq!(default_costs.costs.match_cost(b'A', b'B'), 1.0);
+        assert_eq!(default_costs.costs.match_cost(b'B', b'A'), 1.0);
+        assert_eq!(default_costs.costs.match_cost(b'N', b'K'), 1.0);
+        assert_eq!(default_costs.costs.match_cost(b'C', b'C'), 0.0);
+        assert_eq!(default_costs.costs.gap_ext_cost(), 0.5);
+        assert_eq!(default_costs.costs.gap_open_cost(), 2.5);
+    }
+
+    #[test]
+    fn simple_costs() {
+        let mismatch = 3.0;
+        let gap_open = 2.0;
+        let gap_ext = 10.5;
+        let costs = ParsimonyCostsSimple::new(mismatch, gap_open, gap_ext);
+        assert_eq!(costs.costs.match_cost(b'A', b'B'), mismatch);
+        assert_eq!(costs.costs.match_cost(b'A', b'A'), 0.0);
+        assert_eq!(costs.costs.gap_open_cost(), gap_open * mismatch);
+        assert_eq!(costs.costs.gap_ext_cost(), gap_ext * mismatch);
     }
 }
