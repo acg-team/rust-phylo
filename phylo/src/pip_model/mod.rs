aef2cdf6976514c9a94f39eace18a87bd2a2811d
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2023-11-09 19:56:20 +0100

    Added generic impls for PIPModel
    Added generic implementations of normalise, get_rate, get_p and get_stationary_distribution
    for any size PIP model.
    Added a unified method that makes a PIP matrix from a generic Substitution model.
    Added a specific implementation of the EvolutionaryModel trait for PIP with protein models.

--- phylo/src/pip_model/mod.rs
+++ phylo/src/pip_model/mod.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 
 use anyhow::bail;
-use nalgebra::dvector;
+use nalgebra::{Const, DimMin};
 use ordered_float::OrderedFloat;
 
 use crate::evolutionary_models::EvolutionaryModel;
 use crate::substitution_models::dna_models::nucleotide_index;
+use crate::substitution_models::protein_models::{aminoacid_index, ProteinSubstModel};
 use crate::substitution_models::FreqVector;
 use crate::substitution_models::{dna_models::DNASubstModel, SubstMatrix, SubstitutionModel};
 use crate::{Result, Rounding};
@@ -20,39 +21,13 @@ pub struct PIPModel<const N: usize> {
     pub pi: FreqVector,
 }
 
-// TODO: Make sure Q matrix makes sense like this ALL the time.
-impl EvolutionaryModel<4> for PIPModel<4> {
-    fn new(model_name: &str, model_params: &[f64]) -> Result<Self>
-    where
-        Self: std::marker::Sized,
-    {
-        if model_params.len() < 2 {
-            bail!("Too few values provided for PIP, required 2 values, lambda and mu.");
-        }
-        let lambda = model_params[0];
-        let mu = model_params[1];
-        let subst_model = DNASubstModel::new(model_name, &model_params[2..])?;
-        let mut index = nucleotide_index();
-        index[b'-' as usize] = 4;
-        let mut q = subst_model
-            .q
-            .clone()
-            .insert_column(4, mu)
-            .insert_row(4, 0.0);
-        q.fill_diagonal(0.0);
-        for i in 0..5 {
-            q[(i, i)] = -q.row(i).sum();
-        }
-        let pi = subst_model.pi.clone().insert_row(4, 0.0);
-        let model = PIPModel {
-            index,
-            subst_model,
-            lambda,
-            mu,
-            q,
-            pi,
-        };
-        Ok(model)
+impl<const N: usize> PIPModel<N>
+where
+    Const<N>: DimMin<Const<N>, Output = Const<N>>,
+{
+    fn normalise(&mut self) {
+        let factor = -(self.pi.transpose() * self.q.diagonal())[(0, 0)];
+        self.q /= factor;
     }
 
     fn get_p(&self, time: f64) -> SubstMatrix {
@@ -67,6 +42,88 @@ impl EvolutionaryModel<4> for PIPModel<4> {
         )]
     }
 
+    fn get_stationary_distribution(&self) -> &FreqVector {
+        &self.pi
+    }
+
+    fn make_pip(
+        index: [i32; 255],
+        subst_model: SubstitutionModel<N>,
+        mu: f64,
+        lambda: f64,
+    ) -> PIPModel<N> {
+        let mut index = index;
+        index[b'-' as usize] = N as i32;
+        let mut q = subst_model
+            .q
+            .clone()
+            .insert_column(N, mu)
+            .insert_row(N, 0.0);
+        q.fill_diagonal(0.0);
+        for i in 0..(N + 1) {
+            q[(i, i)] = -q.row(i).sum();
+        }
+        let pi = subst_model.pi.clone().insert_row(N, 0.0);
+        PIPModel {
+            index,
+            subst_model,
+            lambda,
+            mu,
+            q,
+            pi,
+        }
+    }
+
+    fn check_pip_params(model_params: &[f64]) -> Result<(f64, f64)> {
+        if model_params.len() < 2 {
+            bail!("Too few values provided for PIP, required 2 values, lambda and mu.");
+        }
+        let lambda = model_params[0];
+        let mu = model_params[1];
+        Ok((lambda, mu))
+    }
+}
+
+// TODO: Make sure Q matrix makes sense like this ALL the time.
+impl EvolutionaryModel<4> for PIPModel<4> {
+    fn new(model_name: &str, model_params: &[f64]) -> Result<Self>
+    where
+        Self: std::marker::Sized,
+    {
+        let (lambda, mu) = PIPModel::<4>::check_pip_params(model_params)?;
+        let subst_model = DNASubstModel::new(model_name, &model_params[2..])?;
+        let index = nucleotide_index();
+        Ok(PIPModel::make_pip(index, subst_model, mu, lambda))
+    }
+
+    fn normalise(&mut self) {
+        self.normalise();
+    }
+
+    fn get_p(&self, time: f64) -> SubstMatrix {
+        self.get_p(time)
+    }
+
+    fn get_rate(&self, i: u8, j: u8) -> f64 {
+        self.get_rate(i, j)
+    }
+
+    fn get_stationary_distribution(&self) -> &FreqVector {
+        self.get_stationary_distribution()
+    }
+
+    fn get_char_probability(&self, char: u8) -> FreqVector {
+        if char == b'-' {
+            let mut probs = FreqVector::from_column_slice(&[0.0; 5]);
+            probs[4] = 1.0;
+            probs
+        } else {
+            self.subst_model
+                .get_char_probability(char)
+                .insert_row(4, 0.0)
+        }
+    }
+
     fn generate_scorings(
         &self,
         _: &[f64],
@@ -76,29 +133,61 @@ impl EvolutionaryModel<4> for PIPModel<4> {
         unreachable!("This should not be called.")
     }
 
+    fn get_scoring_matrix(&self, _: f64, _: &Rounding) -> (SubstMatrix, f64) {
+        unreachable!("This should not be called.")
+    }
+}
+
+impl EvolutionaryModel<20> for PIPModel<20> {
+    fn new(model_name: &str, model_params: &[f64]) -> Result<Self>
+    where
+        Self: std::marker::Sized,
+    {
+        let (lambda, mu) = PIPModel::<20>::check_pip_params(model_params)?;
+        let subst_model = ProteinSubstModel::new(model_name, &model_params[2..])?;
+        let index = aminoacid_index();
+        Ok(PIPModel::make_pip(index, subst_model, mu, lambda))
+    }
+
     fn normalise(&mut self) {
-        let factor = -(self.pi.transpose() * self.q.diagonal())[(0, 0)];
-        self.q /= factor;
+        self.normalise();
+    }
+
+    fn get_p(&self, time: f64) -> SubstMatrix {
+        self.get_p(time)
+    }
+
+    fn get_rate(&self, i: u8, j: u8) -> f64 {
+        self.get_rate(i, j)
+    }
+
+    fn get_stationary_distribution(&self) -> &FreqVector {
+        self.get_stationary_distribution()
+    }
+
+    fn get_char_probability(&self, char: u8) -> FreqVector {
+        if char == b'-' {
+            let mut probs = FreqVector::from_column_slice(&[0.0; 21]);
+            probs[20] = 1.0;
+            probs
+        } else {
+            self.subst_model
+                .get_char_probability(char)
+                .insert_row(20, 0.0)
+        }
+    }
+
+    fn generate_scorings(
+        &self,
+        _: &[f64],
+        _: bool,
+        _: &Rounding,
+    ) -> HashMap<OrderedFloat<f64>, (SubstMatrix, f64)> {
+        unreachable!("This should not be called.")
     }
 
     fn get_scoring_matrix(&self, _: f64, _: &Rounding) -> (SubstMatrix, f64) {
         unreachable!("This should not be called.")
-    }
-
-    fn get_stationary_distribution(&self) -> &FreqVector {
-        &self.pi
-    }
-
-    fn get_char_probability(&self, char: u8) -> FreqVector {
-        let freq = if char == b'-' {
-            dvector![0.0, 0.0, 0.0, 0.0, 1.0]
-        } else {
-            self.subst_model
-                .get_char_probability(char)
-                .insert_row(4, 0.0)
-        };
-        debug_assert_eq!(freq.len(), 5);
-        freq
     }
 }
 
