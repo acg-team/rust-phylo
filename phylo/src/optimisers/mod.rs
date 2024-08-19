eafec0f076e83d9fb3efe950ff645108a49ab8d3
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2024-08-19 18:22:56 +0200

    New trait for model optim and result struct
    New trait added for model optimisers with a result struct

--- phylo/src/optimisers/mod.rs
+++ phylo/src/optimisers/mod.rs
@@ -1,5 +1,9 @@
 use crate::alignment::Alignment;
+use crate::evolutionary_models::{EvoModel, FrequencyOptimisation};
+use crate::likelihood::LikelihoodCostFunction;
+use crate::phylo_info::PhyloInfo;
 use crate::tree::Tree;
+use crate::Result;
 
 pub mod branch_length_optimiser;
 pub mod dna_model_optimiser;
@@ -18,4 +22,16 @@ pub struct PhyloOptimisationResult {
     pub iterations: usize,
     pub tree: Tree,
     pub alignment: Alignment,
+}
+
+pub struct ModelOptimisationResult<M: EvoModel> {
+    pub initial_logl: f64,
+    pub final_logl: f64,
+    pub iterations: usize,
+    pub model: M,
+}
+
+pub trait ModelOptimiser<'a, LC: LikelihoodCostFunction, M: EvoModel> {
+    fn new(cost: &'a LC, info: &PhyloInfo, frequencies: FrequencyOptimisation) -> Self;
+    fn run(self) -> Result<ModelOptimisationResult<M>>;
 }
