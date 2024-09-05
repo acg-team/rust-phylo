447d9123dae1de2e901c97971fc1038293fba75c
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2024-09-05 16:26:38 +0200

    Tests for protein topology optimisation with pip

--- phylo/src/optimisers/topo_optimiser_tests.rs
+++ phylo/src/optimisers/topo_optimiser_tests.rs
@@ -4,17 +4,14 @@ use approx::assert_relative_eq;
 use bio::io::fasta::Record;
 
 use crate::alignment::Sequences;
-use crate::evolutionary_models::ProteinModelType;
+use crate::evolutionary_models::{DNAModelType::*, EvoModel, ProteinModelType::*};
 use crate::likelihood::PhyloCostFunction;
-use crate::optimisers::PhyloOptimiser;
+// use crate::optimisers::{EvoModelOptimiser, FrequencyOptimisation::*, ModelOptimiser};
+use crate::optimisers::{PhyloOptimiser, TopologyOptimiser};
 use crate::phylo_info::PhyloInfoBuilder;
 use crate::pip_model::PIPModel;
 use crate::substitution_models::{DNASubstModel, ProteinSubstModel};
 use crate::tree::tree_parser::from_newick_string;
-use crate::{
-    evolutionary_models::{DNAModelType::*, EvoModel},
-    optimisers::TopologyOptimiser,
-};
 
 #[test]
 fn primate_topology_from_right_tree() {
@@ -119,7 +116,7 @@ fn protein_topology_optimisation_good_start() {
     // )
     // .build()
     // .unwrap();
-    // let model = ProteinSubstModel::new(ProteinModelType::WAG, &[]).unwrap();
+    // let model = ProteinSubstModel::new(WAG, &[]).unwrap();
     // let unopt_logl = model.cost(&info);
     // let o = TopologyOptimiser::new(&model, &info).run().unwrap();
     // assert!(o.final_logl >= unopt_logl);
@@ -132,7 +129,7 @@ fn protein_topology_optimisation_good_start() {
     )
     .build()
     .unwrap();
-    let model = ProteinSubstModel::new(ProteinModelType::WAG, &[]).unwrap();
+    let model = ProteinSubstModel::new(WAG, &[]).unwrap();
     let logl = model.cost(&info);
 
     // compare the tree height and logl to the output of PhyML
@@ -145,7 +142,7 @@ fn protein_topology_optimisation_nj_start() {
     // let info = PhyloInfoBuilder::new(PathBuf::from("./data/phyml_protein_example/nogap_seqs.fasta"))
     //     .build()
     //     .unwrap();
-    // let model = ProteinSubstModel::new(ProteinModelType::WAG, &[]).unwrap();
+    // let model = ProteinSubstModel::new(WAG, &[]).unwrap();
     // let unopt_logl = model.cost(&info);
     // let o = TopologyOptimiser::new(&model, &info).run().unwrap();
     // assert!(o.final_logl >= unopt_logl);
@@ -158,7 +155,7 @@ fn protein_topology_optimisation_nj_start() {
     )
     .build()
     .unwrap();
-    let model = ProteinSubstModel::new(ProteinModelType::WAG, &[]).unwrap();
+    let model = ProteinSubstModel::new(WAG, &[]).unwrap();
     let logl = model.cost(&info);
 
     // compare the tree height and logl to the output of PhyML
@@ -183,4 +180,83 @@ fn pip_vs_subst_dna_tree() {
     assert!(o_k80.final_logl > initial_logl);
     assert_relative_eq!(o_k80.initial_logl, initial_logl);
     assert_eq!(o_pip.i.tree.robinson_foulds(&o_k80.i.tree), 0);
+}
+
+#[test]
+fn pip_vs_subst_protein_tree_nogaps() {
+    // let info = PhyloInfoBuilder::new(PathBuf::from(
+    //     "./data/phyml_protein_example/nogap_seqs.fasta",
+    // ))
+    // .build()
+    // .unwrap();
+    // let model = PIPModel::<ProteinSubstModel>::new(WAG, &[50.0, 0.1]).unwrap();
+    // let unopt_logl = model.cost(&info);
+    // let o = TopologyOptimiser::new(&model, &info).run().unwrap();
+    // assert!(o.final_logl >= unopt_logl);
+    // The above ran in 308.93s
+
+    let pip_info = PhyloInfoBuilder::with_attrs(
+        PathBuf::from("./data/phyml_protein_example/nogap_seqs.fasta"),
+        PathBuf::from("./data/phyml_protein_example/optimisation_pip_nj_start.newick"),
+    )
+    .build()
+    .unwrap();
+
+    let subst_info = PhyloInfoBuilder::with_attrs(
+        PathBuf::from("./data/phyml_protein_example/nogap_seqs.fasta"),
+        PathBuf::from("./data/phyml_protein_example/optimisation_nj_start.newick"),
+    )
+    .build()
+    .unwrap();
+    // compare the tree created with a substitution model to the one with PIP
+    // the trees are not identical, but very close
+    assert!(pip_info.tree.robinson_foulds(&subst_info.tree) <= 2);
+}
+
+#[test]
+fn protein_optimise_model_tree() {
+    // let info = PhyloInfoBuilder::new(PathBuf::from("./data/phyml_protein_example/seqs.fasta"))
+    //     .build()
+    //     .unwrap();
+    // let model = PIPModel::<ProteinSubstModel>::new(WAG, &[1.4, 0.5]).unwrap();
+    // let unopt_logl = model.cost(&info);
+    // let o = ModelOptimiser::new(&model, &info, Empirical).run().unwrap();
+    // let model_opt_logl = o.final_logl;
+    // assert!(model_opt_logl >= unopt_logl);
+    // let o = TopologyOptimiser::new(&o.model, &info).run().unwrap();
+    // assert!(model_opt_logl >= unopt_logl);
+    // assert!(o.final_logl >= unopt_logl);
+    // // The above ran in 703.87s
+
+    // let info = PhyloInfoBuilder::new(PathBuf::from("./data/phyml_protein_example/seqs.fasta"))
+    //     .build()
+    //     .unwrap();
+    // let model = PIPModel::<ProteinSubstModel>::new(WAG, &[1.4, 0.5]).unwrap();
+    // let unopt_logl = model.cost(&info);
+    // let o = TopologyOptimiser::new(&model, &info).run().unwrap();
+    // assert!(o.final_logl >= unopt_logl);
+    // The above ran in 497.22s
+
+    let optim_model = PIPModel::<ProteinSubstModel>::new(WAG, &[49.56941, 0.09352]).unwrap();
+    let with_model_optim = PhyloInfoBuilder::with_attrs(
+        PathBuf::from("./data/phyml_protein_example/seqs.fasta"),
+        PathBuf::from(
+            "./data/phyml_protein_example/optimisation_pip_nj_start_model_optim_gaps.newick",
+        ),
+    )
+    .build()
+    .unwrap();
+
+    let model = ProteinSubstModel::new(WAG, &[1.4, 0.5]).unwrap();
+    let wo_model_optim = PhyloInfoBuilder::with_attrs(
+        PathBuf::from("./data/phyml_protein_example/seqs.fasta"),
+        PathBuf::from(
+            "./data/phyml_protein_example/optimisation_pip_nj_start_model_optim_gaps.newick",
+        ),
+    )
+    .build()
+    .unwrap();
+
+    assert!(with_model_optim.tree.robinson_foulds(&wo_model_optim.tree) == 0);
+    assert!(optim_model.cost(&with_model_optim) > model.cost(&wo_model_optim));
 }
