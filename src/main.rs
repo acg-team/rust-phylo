c0218bc856470a08f9f61ed1348bf4c20e580e5d
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-02-17 17:43:16 +0100

    New logic for tree search run

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,148 @@
-use crate::cli::Cli;
-use anyhow::Error;
-use clap::Parser;
-use log::LevelFilter;
-use log::{error, info};
-use parsimony::parsimony_alignment::pars_align_on_tree;
-use parsimony::parsimony_alignment::parsimony_costs::parsimony_costs_model::{
-    DNAParsCosts, ProteinParsCosts,
-};
-use phylo::alignment::{self, Alignment};
-use phylo::io;
-use phylo::phylo_info::{setup_phylogenetic_info, PhyloInfo};
-use phylo::sequences::{get_sequence_type, SequenceType};
-use phylo::tree::{get_percentiles, NodeIdx};
-use pretty_env_logger::env_logger::Builder;
-use std::path::PathBuf;
+use std::fmt::Display;
+use std::fs::File;
+use std::io::Write;
 use std::result::Result::Ok;
 
+use anyhow::{bail, Error};
+use clap::Parser;
+
+use log::{debug, info};
+
+use phylo::evolutionary_models::FrequencyOptimisation;
+use phylo::io::write_newick_to_file;
+use phylo::likelihood::{ModelSearchCost, TreeSearchCost};
+use phylo::optimisers::{ModelOptimiser, TopologyOptimiser};
+use phylo::phylo_info::PhyloInfoBuilder;
+use phylo::pip_model::{PIPCostBuilder, PIPModel};
+use phylo::substitution_models::{
+    dna_models::*, protein_models::*, SubstModel, SubstitutionCostBuilder,
+};
+use phylo::tree::Tree;
+
 mod cli;
+use crate::cli::{Cli, ConfigBuilder, GapHandling};
 
 type Result<T> = std::result::Result<T, Error>;
 
-fn cmp_f64() -> impl Fn(&f64, &f64) -> std::cmp::Ordering {
-    |a, b| a.partial_cmp(b).unwrap()
+macro_rules! run_pip_optimisation {
+    ($model:ty, $cfg:expr, $info:expr) => {
+        run_optimisation(
+            PIPCostBuilder::new(PIPModel::<$model>::new(&$cfg.freqs, &$cfg.params)?, $info)
+                .build()?,
+            $cfg.freq_opt,
+            $cfg.max_iters,
+            $cfg.epsilon,
+        )?
+    };
 }
 
-#[cfg(test)]
-fn assert_float_relative_slice_eq(actual: &[f64], expected: &[f64], epsilon: f64) {
-    use approx::relative_eq;
-    assert_eq!(
-        actual.len(),
-        expected.len(),
-        "Must have the same number of entries."
-    );
-    for (i, (&act, &exp)) in actual.iter().zip(expected.iter()).enumerate() {
-        assert!(
-            relative_eq!(exp, act, epsilon = epsilon),
-            "Entries at position {} do not match",
-            i,
-        );
-    }
-}
-
-fn indel_map_align_dna(
-    info: &PhyloInfo,
-    model_name: String,
-    model_params: Vec<f64>,
-    go: f64,
-    ge: f64,
-    categories: u32,
-) -> Result<(Vec<Alignment>, Vec<f64>)> {
-    let scoring = DNAParsCosts::new(
-        &model_name,
-        &model_params,
-        go,
-        ge,
-        &get_percentiles(&info.tree.get_all_branch_lengths(), categories),
-        false,
-        false,
-    )?;
-    Ok(pars_align_on_tree(&Box::new(&scoring), info))
-}
-
-fn indel_map_align_protein(
-    info: &PhyloInfo,
-    model_name: String,
-    _: Vec<f64>,
-    go: f64,
-    ge: f64,
-    categories: u32,
-) -> Result<(Vec<Alignment>, Vec<f64>)> {
-    let scoring = ProteinParsCosts::new(
-        &model_name,
-        go,
-        ge,
-        &get_percentiles(&info.tree.get_all_branch_lengths(), categories),
-        false,
-        false,
-    )?;
-    Ok(pars_align_on_tree(&Box::new(&scoring), info))
+macro_rules! run_subst_optimisation {
+    ($model:ty, $cfg:expr, $info:expr) => {
+        run_optimisation(
+            SubstitutionCostBuilder::new(
+                SubstModel::<$model>::new(&$cfg.freqs, &$cfg.params)?,
+                $info,
+            )
+            .build()?,
+            $cfg.freq_opt,
+            $cfg.max_iters,
+            $cfg.epsilon,
+        )?
+    };
 }
 
 fn main() -> Result<()> {
-    Builder::new()
-        .filter_level(LevelFilter::Info)
-        .format_timestamp_secs()
-        .format_module_path(false)
-        .init();
-    info!("JATI run started");
-    let cli = Cli::try_parse()?;
-    info!("Successfully parsed the command line parameters");
-    let info = setup_phylogenetic_info(cli.seq_file, cli.tree_file);
-    match info {
-        Ok(info) => {
-            match cli.command {
-                Some(command) => match command {
-                    cli::Commands::IndelMAP { go, ge, categories } => {
-                        let (alignment, scores) = match get_sequence_type(&info.sequences) {
-                            SequenceType::DNA => {
-                                info!("Working on DNA data -- please ensure that data type is inferred correctly.");
-                                indel_map_align_dna(
-                                    &info,
-                                    cli.model,
-                                    cli.model_params,
-                                    go,
-                                    ge,
-                                    categories,
-                                )?
-                            }
-                            SequenceType::Protein => {
-                                info!("Working on protein data -- please ensure that data type is inferred correctly.");
-                                indel_map_align_protein(
-                                    &info,
-                                    cli.model,
-                                    cli.model_params,
-                                    go,
-                                    ge,
-                                    categories,
-                                )?
-                            }
-                        };
-                        info!("Final alignment scores are: \n{:?}", scores);
-                        let out_msa_path = match cli.output_msa_file {
-                            Some(path) => path,
-                            None => {
-                                let path = PathBuf::from("msa.fasta");
-                                info!(
-                                    "No output file name provided, writing MSA to default file {}.",
-                                    path.display()
-                                );
-                                path
-                            }
-                        };
-                        io::write_sequences_to_file(
-                            &alignment::compile_alignment_representation(
-                                &info,
-                                &alignment,
-                                None::<NodeIdx>,
-                            ),
-                            out_msa_path,
-                        )?;
-                        info!("IndelMAP alignment done, quitting.");
-                    }
-                },
-                None => {
-                    info!("Can only do IndelMAP alignments at the moment, quitting.");
-                    return Ok(());
-                }
-            };
+    let cli = match Cli::try_parse() {
+        Ok(cli) => {
+            info!("Successfully parsed the command line parameters");
+            cli
         }
         Err(error) => {
-            error!("Error in input data: {}", error);
-            return Ok(());
+            bail!("Unable to parse command line arguments: \n {}", error)
         }
+    };
+    let cfg_build: ConfigBuilder = cli.into();
+    let cfg = cfg_build.setup()?;
+
+    info!("JATI run started.");
+    info!("{}", cfg);
+
+    info!("Running on sequences from {}.", cfg.seq_file.display());
+    match &cfg.input_tree {
+        Some(tree_file) => info!("Using tree from {}.", tree_file.display()),
+        None => info!("No input tree provided, building NJ tree from sequences."),
     }
+    let info = PhyloInfoBuilder::new(cfg.seq_file)
+        .tree_file(cfg.input_tree)
+        .build()?;
+
+    let (cost, tree) = match cfg.gap_handling {
+        GapHandling::PIP => {
+            info!("Gap handling: PIP.");
+            match cfg.model.as_str() {
+                "JC69" => run_pip_optimisation!(JC69, cfg, info),
+                "K80" => run_pip_optimisation!(K80, cfg, info),
+                "HKY85" | "HKY" => run_pip_optimisation!(HKY, cfg, info),
+                "TN93" => run_pip_optimisation!(TN93, cfg, info),
+                "GTR" => run_pip_optimisation!(GTR, cfg, info),
+                "WAG" => run_pip_optimisation!(WAG, cfg, info),
+                "HIVB" => run_pip_optimisation!(HIVB, cfg, info),
+                "BLOSUM" => run_pip_optimisation!(BLOSUM, cfg, info),
+                _ => bail!("Unknown model: {}", cfg.model),
+            }
+        }
+        GapHandling::Missing => {
+            info!("Gap handling: as missing data.");
+            match cfg.model.as_str() {
+                "JC69" => run_subst_optimisation!(JC69, cfg, info),
+                "K80" => run_subst_optimisation!(K80, cfg, info),
+                "HKY85" | "HKY" => run_subst_optimisation!(HKY, cfg, info),
+                "TN93" => run_subst_optimisation!(TN93, cfg, info),
+                "GTR" => run_subst_optimisation!(GTR, cfg, info),
+                "WAG" => run_subst_optimisation!(WAG, cfg, info),
+                "HIVB" => run_subst_optimisation!(HIVB, cfg, info),
+                "BLOSUM" => run_subst_optimisation!(BLOSUM, cfg, info),
+                _ => bail!("Unknown model: {}", cfg.model),
+            }
+        }
+    };
+
+    info!("Putting resulting tree in {}", cfg.out_tree.display());
+
+    write_newick_to_file(&[tree.clone()], cfg.out_tree)?;
+    let mut out = File::create(cfg.out_fldr.join("log.out"))?;
+    writeln!(out, "{}", cost)?;
+
     Ok(())
+}
+
+fn run_optimisation(
+    cost: impl TreeSearchCost + ModelSearchCost + Display + Clone,
+    freq_opt: FrequencyOptimisation,
+    max_iterations: usize,
+    epsilon: f64,
+) -> Result<(f64, Tree)> {
+    let mut cost = cost;
+    let mut prev_cost = f64::NEG_INFINITY;
+    let mut final_cost = TreeSearchCost::cost(&cost);
+
+    let mut iterations = 0;
+    while final_cost - prev_cost > epsilon && iterations < max_iterations {
+        iterations += 1;
+        info!("Iteration: {}", iterations);
+
+        prev_cost = final_cost;
+        let model_optimiser = ModelOptimiser::new(cost, freq_opt);
+        let o = TopologyOptimiser::new(model_optimiser.run()?.cost)
+            .run()
+            .unwrap();
+        final_cost = o.final_logl;
+        cost = o.cost;
+    }
+
+    info!("Final cost after {} iterations: {}", iterations, final_cost);
+    debug!("Final parameters: {:?}", cost.params());
+    debug!("Final frequencies: {:?}", cost.freqs());
+    debug!("Final tree: {}", cost.tree());
+    Ok((final_cost, cost.tree().clone()))
 }
