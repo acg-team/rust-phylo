cf1e92d4d43f8fe4a20fe1d0888f6f28f87b8091
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-02-17 17:42:17 +0100

    CLI update + configuration struct

--- src/cli.rs
+++ src/cli.rs
@@ -1,48 +1,187 @@
-use clap::{Parser, Subcommand};
+use std::fmt::{self, Display};
 use std::path::PathBuf;
 
+use clap::Parser;
+use ftail::Ftail;
+use log::LevelFilter;
+
+use phylo::evolutionary_models::FrequencyOptimisation;
+
+use crate::Result;
+
+#[derive(Clone, clap::ValueEnum, Copy)]
+#[allow(clippy::upper_case_acronyms)]
+pub(super) enum GapHandling {
+    PIP,
+    Missing,
+}
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub(super) struct Cli {
+    /// Output folder
+    #[arg(short = 'd', long, value_name = "OUTPUT_FOLDER", default_value = ".")]
+    pub(super) out_folder: PathBuf,
+
+    /// Run identifier for output
+    #[arg(short, long, value_name = "RUN_NAME")]
+    pub(super) run_name: Option<String>,
+
+    /// Max iterations for the optimisation
+    #[arg(short = 'x', long, value_name = "MAX_ITERATIONS", default_value = "5")]
+    pub(super) max_iterations: usize,
+
     /// Sequence file in fasta format
     #[arg(short, long, value_name = "SEQ_FILE")]
     pub(super) seq_file: PathBuf,
 
     /// Tree file in newick format
     #[arg(short, long, value_name = "TREE_FILE")]
-    pub(super) tree_file: PathBuf,
-
-    /// Tree file in newick format
-    #[arg(short, long, value_name = "OUTPUT_MSA_FILE")]
-    pub(super) output_msa_file: Option<PathBuf>,
+    pub(super) tree_file: Option<PathBuf>,
 
     /// Sequence evolution model
     #[arg(short, long, value_name = "MODEL", rename_all = "UPPER")]
     pub(super) model: String,
 
-    /// Sequence evolution model parameters, e.g. alpha and beta for k80 and 
-    /// f_t f_c f_a f_g r_tc r_ta r_tg r_ca r_cg r_ag for GTR (in this specific order)
+    /// Sequence evolution model parameters, e.g. alpha for k80 and
+    /// r_tc r_ta r_tg r_ca r_cg r_ag for GTR (in this order)
     #[arg(short = 'p', long, value_name = "MODEL_PARAMS", num_args = 0..)]
-    pub(super) model_params: Vec<f64>,
+    pub(super) params: Vec<f64>,
 
-    #[command(subcommand)]
-    pub(super) command: Option<Commands>,
+    /// Sequence evolution model stationary frequencies
+    /// pi_t pi_c pi_a pi_g (in this order)
+    #[arg(short = 'f', long, value_name = "FREQUENCIES", num_args = 0..)]
+    pub(super) freqs: Vec<f64>,
+
+    /// Frequency optimisation method: fixed, empirical, estimated
+    #[arg(
+        short = 'o',
+        long,
+        value_name = "FREQ_OPTIMISATION",
+        default_value = "empirical"
+    )]
+    pub(super) freq_opt: FrequencyOptimisation,
+
+    /// Gap handling method: using the PIP model or treating gaps as missing data
+    #[arg(short, long, value_name = "GAP_HANDLING")]
+    pub(super) gap_handling: GapHandling,
+
+    /// Epsilon value for numerical optimisation
+    #[arg(short, long, value_name = "EPSILON", default_value = "1e-2")]
+    pub(super) epsilon: f64,
 }
 
-#[derive(Subcommand)]
-pub(super) enum Commands {
-    /// Aligns the given sequences with the IndelMaP algorithm
-    IndelMAP {
-        /// Gap opening penalty
-        #[arg(short = 'o', long, default_value_t = 2.5)]
-        go: f64,
+pub struct ConfigBuilder {
+    pub timestamp: chrono::NaiveDateTime,
+    pub out_path: PathBuf,
+    pub run_name: Option<String>,
+    pub max_iters: usize,
+    pub seq_file: PathBuf,
+    pub input_tree: Option<PathBuf>,
+    pub model: String,
+    pub params: Vec<f64>,
+    pub freqs: Vec<f64>,
+    pub freq_opt: FrequencyOptimisation,
+    pub gap_handling: GapHandling,
+    pub epsilon: f64,
+}
 
-        /// Gap extension penalty
-        #[arg(short = 'e', long, default_value_t = 0.5)]
-        ge: f64,
+impl From<Cli> for ConfigBuilder {
+    fn from(cli: Cli) -> Self {
+        ConfigBuilder {
+            timestamp: chrono::Utc::now().naive_utc(),
+            out_path: cli.out_folder,
+            run_name: cli.run_name,
+            max_iters: cli.max_iterations,
+            seq_file: cli.seq_file,
+            input_tree: cli.tree_file,
+            model: cli.model,
+            params: cli.params,
+            freqs: cli.freqs,
+            freq_opt: cli.freq_opt,
+            gap_handling: cli.gap_handling,
+            epsilon: cli.epsilon,
+        }
+    }
+}
 
-        /// Number of percentile categories to use for branch length approximation
-        #[arg(short, long, default_value_t = 4)]
-        categories: u32,
-    },
+pub struct Config {
+    pub timestamp: chrono::NaiveDateTime,
+    pub out_fldr: PathBuf,
+    pub out_tree: PathBuf,
+    pub run_id: String,
+    pub max_iters: usize,
+    pub seq_file: PathBuf,
+    pub input_tree: Option<PathBuf>,
+    pub model: String,
+    pub params: Vec<f64>,
+    pub freqs: Vec<f64>,
+    pub freq_opt: FrequencyOptimisation,
+    pub gap_handling: GapHandling,
+    pub epsilon: f64,
+}
+
+impl Display for Config {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Run start time: {}", self.timestamp)?;
+        writeln!(f, "Run ID: {}", self.run_id)?;
+
+        writeln!(f, "Input sequence file: {}", self.seq_file.display())?;
+        match self.input_tree {
+            Some(ref tree_file) => writeln!(f, "Input tree file: {}", tree_file.display())?,
+            None => writeln!(f, "No input tree file provided.")?,
+        }
+        writeln!(f, "Output folder: {}", self.out_fldr.display())?;
+
+        let overmodel = match self.gap_handling {
+            GapHandling::PIP => "PIP",
+            GapHandling::Missing => "Substitution",
+        };
+        writeln!(f, "Model setup: {} model with {} Q ", overmodel, self.model)?;
+        writeln!(f, "Model parameters: {:?}", self.params)?;
+        writeln!(f, "Model frequencies: {:?}", self.freqs)?;
+
+        writeln!(
+            f,
+            "Optimisation setup: frequencies: {:#?}, max iterations: {}, epsilon: {}",
+            self.freq_opt, self.max_iters, self.epsilon
+        )
+    }
+}
+
+impl ConfigBuilder {
+    pub(crate) fn setup(self) -> Result<Config> {
+        let run_id = self.run_name.as_deref().map_or_else(
+            || self.timestamp.to_string(),
+            |run_name| format!("{}_{}", run_name, self.timestamp),
+        );
+        let out_fldr = self.out_path.join(format!("{}_output", run_id));
+        std::fs::create_dir_all(&out_fldr)?;
+        Ftail::new()
+            .datetime_format("%H:%M:%S%.3f")
+            .console(LevelFilter::Info)
+            .single_file(
+                out_fldr.join("jati.log").to_str().unwrap(),
+                true,
+                LevelFilter::Debug,
+            )
+            .init()?;
+
+        let out_tree = out_fldr.join(format!("{}_tree.newick", run_id));
+
+        Ok(Config {
+            timestamp: self.timestamp,
+            out_fldr,
+            out_tree,
+            run_id,
+            max_iters: self.max_iters,
+            seq_file: self.seq_file,
+            input_tree: self.input_tree,
+            model: self.model,
+            params: self.params,
+            freqs: self.freqs,
+            freq_opt: self.freq_opt,
+            gap_handling: self.gap_handling,
+            epsilon: self.epsilon,
+        })
+    }
 }
