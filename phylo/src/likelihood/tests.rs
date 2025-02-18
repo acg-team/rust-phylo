9c214197452f399500dada4b70be7a8a8e70a7df
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-02-18 17:36:16 +0100

    Added test for model/data alphabet mismatch

--- phylo/src/likelihood/tests.rs
+++ phylo/src/likelihood/tests.rs
@@ -134,3 +134,89 @@ fn protein_pip_search_costs_equal() {
         &[1.2, 0.5],
     ));
 }
+
+#[cfg(test)]
+fn alphabet_mismatch_subst_model_template<Q: QMatrix + QMatrixMaker>(
+    alpha: Alphabet,
+    freqs: &[f64],
+    params: &[f64],
+) {
+    // https://molevolworkshop.github.io/faculty/huelsenbeck/pdf/WoodsHoleHandout.pdf
+    let fldr = Path::new("./data");
+    let records =
+        read_sequences_from_file(&fldr.join("Huelsenbeck_example_long_DNA.fasta")).unwrap();
+    let seqs = Sequences::with_alphabet(records.clone(), alpha);
+    let tree = tree!(&fs::read_to_string(fldr.join("Huelsenbeck_example.newick")).unwrap());
+    let info = PIB::build_from_objects(seqs, tree).unwrap();
+    let model = SubstModel::<Q>::new(freqs, params).unwrap();
+    let res = SCB::new(model, info).build();
+    assert!(res.is_err());
+    assert!(res.err().unwrap().to_string().contains("Alphabet mismatch"));
+}
+
+#[test]
+fn alphabet_mismatch_subst_model() {
+    alphabet_mismatch_subst_model_template::<JC69>(protein_alphabet(), &[], &[]);
+    alphabet_mismatch_subst_model_template::<K80>(protein_alphabet(), &[], &[2.0]);
+    alphabet_mismatch_subst_model_template::<HKY>(
+        protein_alphabet(),
+        &[0.22, 0.26, 0.33, 0.19],
+        &[0.5],
+    );
+    alphabet_mismatch_subst_model_template::<TN93>(
+        protein_alphabet(),
+        &[0.22, 0.26, 0.33, 0.19],
+        &[0.5970915, 0.2940435, 0.00135],
+    );
+    alphabet_mismatch_subst_model_template::<GTR>(
+        protein_alphabet(),
+        &[0.1, 0.3, 0.4, 0.2],
+        &[1.5; 5],
+    );
+    alphabet_mismatch_subst_model_template::<WAG>(dna_alphabet(), &[], &[]);
+    alphabet_mismatch_subst_model_template::<BLOSUM>(dna_alphabet(), &[], &[]);
+    alphabet_mismatch_subst_model_template::<HIVB>(dna_alphabet(), &[], &[]);
+}
+
+#[cfg(test)]
+fn alphabet_mismatch_subst_pip_template<Q: QMatrix + QMatrixMaker>(
+    alpha: Alphabet,
+    freqs: &[f64],
+    params: &[f64],
+) {
+    // https://molevolworkshop.github.io/faculty/huelsenbeck/pdf/WoodsHoleHandout.pdf
+    let fldr = Path::new("./data");
+    let records =
+        read_sequences_from_file(&fldr.join("Huelsenbeck_example_long_DNA.fasta")).unwrap();
+    let seqs = Sequences::with_alphabet(records.clone(), alpha);
+    let tree = tree!(&fs::read_to_string(fldr.join("Huelsenbeck_example.newick")).unwrap());
+    let info = PIB::build_from_objects(seqs, tree).unwrap();
+    let model = PIPModel::<Q>::new(freqs, params).unwrap();
+    let res = PIPCB::new(model, info).build();
+    assert!(res.is_err());
+    assert!(res.err().unwrap().to_string().contains("Alphabet mismatch"));
+}
+
+#[test]
+fn alphabet_mismatch_pip_model() {
+    alphabet_mismatch_subst_pip_template::<JC69>(protein_alphabet(), &[], &[1.3, 0.5]);
+    alphabet_mismatch_subst_pip_template::<K80>(protein_alphabet(), &[], &[1.3, 0.5, 2.0]);
+    alphabet_mismatch_subst_pip_template::<HKY>(
+        protein_alphabet(),
+        &[0.22, 0.26, 0.33, 0.19],
+        &[1.3, 0.5, 0.5],
+    );
+    alphabet_mismatch_subst_pip_template::<TN93>(
+        protein_alphabet(),
+        &[0.22, 0.26, 0.33, 0.19],
+        &[1.3, 0.5, 0.5970915, 0.2940435, 0.00135],
+    );
+    alphabet_mismatch_subst_pip_template::<GTR>(
+        protein_alphabet(),
+        &[0.1, 0.3, 0.4, 0.2],
+        &[1.5; 7],
+    );
+    alphabet_mismatch_subst_pip_template::<WAG>(dna_alphabet(), &[], &[1.3, 0.5]);
+    alphabet_mismatch_subst_pip_template::<BLOSUM>(dna_alphabet(), &[], &[1.3, 0.5]);
+    alphabet_mismatch_subst_pip_template::<HIVB>(dna_alphabet(), &[], &[1.3, 0.5]);
+}
