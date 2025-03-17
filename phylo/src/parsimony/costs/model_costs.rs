44b74da127f756b8c78f456fbdaa28952ca86e3a
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-03-17 14:18:43 +0100

    Trying to make getting costs per branch efficient
    Using a Vec<(OrderedFloat<f64>,BranchModelCost)> to store costs per branch length and binary search
    for the closest one when requenced. Seems to be the fastest way to do this given that we have a predefined
    set of times that we need to have individual scorings for, but branch lengths may change.

--- phylo/src/parsimony/costs/model_costs.rs
+++ phylo/src/parsimony/costs/model_costs.rs
@@ -1,106 +1,14 @@
-use std::collections::HashMap;
-
 use log::{debug, info};
+use ordered_float::OrderedFloat;
 
 use crate::alphabets::Alphabet;
-use crate::parsimony::{CostMatrix, GapCost, ParsimonyCosts, ParsimonyModel, Rounding, Zeroing};
-use crate::{cmp_f64, ord_f64, Result};
+use crate::parsimony::{CostMatrix, DiagonalZeros, GapCost, ParsimonyCosts, ParsimonyModel, Rounding};
+use crate::Result;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ModelCosts {
     alphabet: Alphabet,
-    times: Vec<f64>,
-    costs: HashMap<ord_f64, ModelBranchCosts>,
-}
-
-impl ModelCosts {
-    pub fn new(
-        model: &dyn ParsimonyModel,
-        gap: GapCost,
-        diagonal_zeros: Zeroing,
-        rounding: Rounding,
-        times: &[f64],
-    ) -> Result<Self> {
-        info!(
-            "Setting up the parsimony scoring from the {} substitution model.",
-            model
-        );
-
-        let mut costs = HashMap::<ord_f64, ModelBranchCosts>::with_capacity(times.len());
-
-        for time in times {
-            let (cost_matrix, avg) =
-                model.scoring_matrix_corrected(*time, diagonal_zeros.clone(), rounding.clone());
-            costs.insert(
-                ord_f64::from(*time),
-                ModelBranchCosts {
-                    avg,
-                    gap: gap.clone() * avg,
-                    c: cost_matrix,
-                },
-            );
-        }
-
-        info!(
-            "Created scoring matrices from the {} substitution model for {:?} branch lengths.",
-            model, times
-        );
-        debug!("The scoring matrices are: {:?}", costs);
-        Ok(ModelCosts {
-            alphabet: model.alphabet().clone(),
-            times: sort_times(times),
-            costs,
-        })
-    }
-}
-
-fn sort_times(times: &[f64]) -> Vec<f64> {
-    let mut sorted_times = Vec::from(times);
-    sorted_times.sort_by(cmp_f64());
-    sorted_times
-}
-
-impl ModelCosts {
-    fn find_closest_branch_length(&self, target: f64) -> f64 {
-        debug!("Getting scoring for time {}", target);
-        let time = match self
-            .times
-            .windows(2)
-            .filter(|&window| target - window[0] > window[1] - target)
-            .last()
-        {
-            Some(window) => window[1],
-            None => self.times[0],
-        };
-        debug!("Using scoring for time {}", time);
-        time
-    }
-}
-
-impl ParsimonyCosts for ModelCosts {
-    fn r#match(&self, blen: f64, i: &u8, j: &u8) -> f64 {
-        self.costs[&ord_f64::from(self.find_closest_branch_length(blen))].c
-            [(self.alphabet.index(i), self.alphabet.index(j))]
-    }
-
-    fn gap_open(&self, blen: f64) -> f64 {
-        self.costs[&ord_f64::from(self.find_closest_branch_length(blen))]
-            .gap
-            .open
-    }
-
-    fn gap_ext(&self, blen: f64) -> f64 {
-        self.costs[&ord_f64::from(self.find_closest_branch_length(blen))]
-            .gap
-            .ext
-    }
-
-    fn avg(&self, blen: f64) -> f64 {
-        self.costs
-            .get(&ord_f64::from(self.find_closest_branch_length(blen)))
-            .unwrap()
-            .avg
-    }
+    costs: Vec<(OrderedFloat<f64>, ModelBranchCosts)>,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -110,16 +18,105 @@ pub(crate) struct ModelBranchCosts {
     c: CostMatrix,
 }
 
+impl ModelCosts {
+    pub fn new(
+        model: &dyn ParsimonyModel,
+        gap: GapCost,
+        diagonal: DiagonalZeros,
+        rounding: Rounding,
+        times: &[f64],
+    ) -> Result<Self> {
+        info!(
+            "Setting up the parsimony scoring from the {} substitution model.",
+            model
+        );
+
+        let mut times = times
+            .iter()
+            .cloned()
+            .map(OrderedFloat::<f64>::from)
+            .collect::<Vec<_>>();
+        times.sort();
+
+        let costs: Vec<(OrderedFloat<f64>, ModelBranchCosts)> = times
+            .iter()
+            .map(|time| {
+                let (cost_matrix, avg) =
+                    model.scoring_corrected(f64::from(*time), diagonal.clone(), rounding.clone());
+                (
+                    *time,
+                    ModelBranchCosts {
+                        avg,
+                        gap: gap.clone() * avg,
+                        c: cost_matrix,
+                    },
+                )
+            })
+            .collect();
+
+        info!(
+            "Created scoring matrices from the {} substitution model for {:?} branch lengths.",
+            model, times
+        );
+        debug!("The scoring matrices are: {:?}", costs);
+        Ok(ModelCosts {
+            alphabet: model.alphabet().clone(),
+            costs,
+        })
+    }
+}
+
+impl ModelCosts {
+    fn scoring(&self, target: OrderedFloat<f64>) -> &ModelBranchCosts {
+        let target = OrderedFloat(target);
+        match self.costs.binary_search_by(|(time, _)| time.cmp(&target)) {
+            Ok(idx) => &self.costs[idx].1, // Exact match
+            Err(idx) => {
+                if idx == 0 {
+                    &self.costs[0].1
+                } else if idx == self.costs.len() {
+                    &self.costs[self.costs.len() - 1].1
+                } else {
+                    // Pick closest value
+                    let (before, _) = self.costs[idx - 1];
+                    let (after, _) = self.costs[idx];
+                    if (target.0 - before.0).abs() <= (after - target.0).abs() {
+                        &self.costs[idx - 1].1
+                    } else {
+                        &self.costs[idx].1
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl ParsimonyCosts for ModelCosts {
+    fn r#match(&self, blen: f64, i: &u8, j: &u8) -> f64 {
+        self.scoring(OrderedFloat(blen)).c[(self.alphabet.index(i), self.alphabet.index(j))]
+    }
+
+    fn gap_open(&self, blen: f64) -> f64 {
+        self.scoring(OrderedFloat(blen)).gap.open
+    }
+
+    fn gap_ext(&self, blen: f64) -> f64 {
+        self.scoring(OrderedFloat(blen)).gap.ext
+    }
+
+    fn avg(&self, blen: f64) -> f64 {
+        self.scoring(OrderedFloat(blen)).avg
+    }
+}
+
 #[cfg(test)]
 mod private_tests {
     use approx::assert_relative_eq;
 
-    use crate::parsimony::costs::ParsimonyCosts;
-    use crate::parsimony::{CostMatrix, ParsimonyModel, Rounding as R};
     use crate::substitution_models::{SubstModel, HIVB, K80, WAG};
 
-    use super::Zeroing as Z;
     use super::*;
+    use super::{DiagonalZeros as Z, Rounding as R};
 
     const TRUE_COST_MATRIX: [f64; 400] = [
         0.0, 6.0, 6.0, 5.0, 6.0, 6.0, 5.0, 4.0, 7.0, 7.0, 6.0, 5.0, 6.0, 7.0, 5.0, 4.0, 4.0, 9.0,
@@ -149,59 +146,55 @@ mod private_tests {
 
     #[test]
     fn generate_protein_scorings() {
-        let model = SubstModel::<WAG>::new(&[], &[]);
-        let scoring = ModelCosts::new(
-            &model,
+        let s = ModelCosts::new(
+            &SubstModel::<WAG>::new(&[], &[]),
             GapCost::new(2.5, 1.0),
-            Z::no(),
+            Z::non_zero(),
             R::zero(),
             &[0.1, 0.3, 0.5, 0.7],
         )
         .unwrap();
 
-        let mat_01 = scoring
-            .costs
-            .get(&ordered_float::OrderedFloat(0.1))
-            .unwrap();
+        let mat_01 = s.scoring(OrderedFloat(0.1));
         let true_matrix_01 = CostMatrix::from_row_slice(20, 20, &TRUE_COST_MATRIX);
         assert_relative_eq!(mat_01.c, true_matrix_01);
         assert_relative_eq!(mat_01.avg, 5.7675);
 
-        assert_relative_eq!(scoring.avg(0.3), 4.7475);
-        assert_relative_eq!(scoring.avg(0.5), 4.2825);
-        assert_relative_eq!(scoring.avg(0.7), 4.0075);
+        assert_relative_eq!(s.avg(0.3), 4.7475);
+        assert_relative_eq!(s.avg(0.5), 4.2825);
+        assert_relative_eq!(s.avg(0.7), 4.0075);
     }
 
     #[test]
     fn protein_scoring_matrices() {
         let model = SubstModel::<WAG>::new(&[], &[]);
         let true_matrix_01 = CostMatrix::from_row_slice(20, 20, &TRUE_COST_MATRIX);
-        let (mat, avg) = ParsimonyModel::scoring_matrix(&model, 0.1, R::zero());
+        let (mat, avg) = model.scoring(0.1, R::zero());
 
         assert_relative_eq!(mat, true_matrix_01);
 
         assert_relative_eq!(avg, 5.7675);
-        let (_, avg) = ParsimonyModel::scoring_matrix(&model, 0.3, R::zero());
+        let (_, avg) = model.scoring(0.3, R::zero());
         assert_relative_eq!(avg, 4.7475);
-        let (_, avg) = ParsimonyModel::scoring_matrix(&model, 0.5, R::zero());
+        let (_, avg) = model.scoring(0.5, R::zero());
         assert_relative_eq!(avg, 4.2825);
-        let (_, avg) = ParsimonyModel::scoring_matrix(&model, 0.7, R::zero());
+        let (_, avg) = model.scoring(0.7, R::zero());
         assert_relative_eq!(avg, 4.0075);
     }
 
     #[test]
     fn matrix_entry_rounding() {
         let model = SubstModel::<K80>::new(&[], &[1.0, 2.0]);
-        let (mat_round, avg_round) = model.scoring_matrix_corrected(0.1, Z::yes(), R::zero());
-        let (mat, avg) = model.scoring_matrix_corrected(0.1, Z::yes(), R::none());
+        let (mat_round, avg_round) = model.scoring_corrected(0.1, Z::zero(), R::zero());
+        let (mat, avg) = model.scoring_corrected(0.1, Z::zero(), R::none());
         assert_ne!(avg_round, avg);
         assert_ne!(mat_round, mat);
         for &element in mat_round.as_slice() {
             assert_eq!(element.round(), element);
         }
         let model = SubstModel::<HIVB>::new(&[], &[]);
-        let (mat_round, avg_round) = model.scoring_matrix_corrected(0.1, Z::yes(), R::zero());
-        let (mat, avg) = model.scoring_matrix_corrected(0.1, Z::yes(), R::none());
+        let (mat_round, avg_round) = model.scoring_corrected(0.1, Z::zero(), R::zero());
+        let (mat, avg) = model.scoring_corrected(0.1, Z::zero(), R::none());
         assert_ne!(avg_round, avg);
         assert_ne!(mat_round, mat);
         for &element in mat_round.as_slice() {
@@ -212,8 +205,8 @@ mod private_tests {
     #[test]
     fn matrix_zero_diagonals() {
         let model = SubstModel::<HIVB>::new(&[], &[]);
-        let (mat_zeros, avg_zeros) = model.scoring_matrix_corrected(0.5, Z::yes(), R::zero());
-        let (mat, avg) = model.scoring_matrix_corrected(0.5, Z::no(), R::zero());
+        let (mat_zeros, avg_zeros) = model.scoring_corrected(0.5, Z::zero(), R::zero());
+        let (mat, avg) = model.scoring_corrected(0.5, Z::non_zero(), R::zero());
         assert_ne!(avg_zeros, avg);
         assert!(avg_zeros < avg);
         assert_ne!(mat_zeros, mat);
