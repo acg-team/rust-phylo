a78aacd6b96020f45dd2b6fdddcd50ce3c68a982
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-03-19 19:20:07 +0100

    AlignmentBuilder now always aligns unaligned seqs
    AlignmentBuilder now only aligns unaligned sequences using either default simple costs or
    the provided parsimony costs. Aligned sequences now get processed by Alignment directly.

--- phylo/src/alignment/alignment_builder.rs
+++ phylo/src/alignment/alignment_builder.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
 
 use crate::alignment::{Alignment, Sequences};
-
-use crate::parsimony::costs::{GapCost, SimpleCosts};
+use crate::parsimony::costs::{GapCost, ParsimonyCosts, SimpleCosts};
 use crate::parsimony::pars_align_on_tree;
 use crate::tree::Tree;
 use crate::Result;
@@ -18,13 +17,25 @@ impl<'a> AlignmentBuilder<'a> {
     }
 
     pub fn build(self) -> Result<Alignment> {
-        self.align_unaligned_seqs()
+        let costs = SimpleCosts::new(1.0, GapCost::new(2.5, 0.5));
+        let (aligns, _scores) = pars_align_on_tree(&costs, self.tree, self.seqs.clone());
+
+        let mut alignment = Alignment {
+            seqs: self.seqs.into_gapless(),
+            leaf_map: HashMap::new(),
+            node_map: aligns,
+            leaf_encoding: HashMap::new(),
+        };
+        let leaf_map = alignment
+            .compile_leaf_map(&self.tree.root, self.tree)
+            .unwrap();
+        alignment.leaf_map = leaf_map;
+        alignment.leaf_encoding = alignment.seqs.generate_leaf_encoding();
+        Ok(alignment)
     }
 
-    fn align_unaligned_seqs(self) -> Result<Alignment> {
-        let gap = GapCost::new(2.5, 0.5);
-        let costs = SimpleCosts::new(1.0, gap);
-        let (aligns, _scores) = pars_align_on_tree(&costs, self.tree, self.seqs.clone());
+    pub fn build_with_costs(self, costs: &dyn ParsimonyCosts) -> Result<Alignment> {
+        let (aligns, _scores) = pars_align_on_tree(costs, self.tree, self.seqs.clone());
         let mut alignment = Alignment {
             seqs: Sequences::new(Vec::new()),
             leaf_map: HashMap::new(),
