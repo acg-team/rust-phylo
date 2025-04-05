bc24a9dbcd6597fb36eae48156879dc5590119cf
= <merlin@merlinio.ch>
2025-04-05 23:16:08 +0200

    adapt cost benchmark to new datasets

--- phylo/benches/cost.rs
+++ phylo/benches/cost.rs
@@ -1,32 +1,60 @@
-use std::hint::black_box;
+use std::{hint::black_box, path::PathBuf};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use phylo::{
     likelihood::ModelSearchCost,
     phylo_info::PhyloInfoBuilder,
-    pip_model::{PIPCostBuilder, PIPModel},
-    substitution_models::JC69,
+    pip_model::{PIPCost, PIPCostBuilder, PIPModel},
+    substitution_models::{QMatrix, QMatrixMaker, JC69},
 };
 
-fn pip_cost_tiny_but_long(criterion: &mut Criterion) {
-    let info = black_box(
-        PhyloInfoBuilder::new("data/sim/GTR/gtr.fasta".into())
-            .build()
-            .unwrap(),
-    );
-    let jc69 = black_box(PIPModel::<JC69>::new(&[], &[]));
-    let c = black_box(PIPCostBuilder::new(jc69, info).build().unwrap());
-    criterion.bench_function("PIP cost tiny but long", |bench| {
-        bench.iter(|| {
+fn setup<Q: QMatrix + QMatrixMaker>(seq_path: impl Into<PathBuf>) -> PIPCost<Q> {
+    let info = black_box(PhyloInfoBuilder::new(seq_path.into()).build().unwrap());
+    let jc69 = black_box(PIPModel::<Q>::new(&[], &[]));
+    black_box(PIPCostBuilder::new(jc69, info).build().unwrap())
+}
+
+fn pip_cost_dna_easy_small(criterion: &mut Criterion) {
+    let c = setup::<JC69>("data/benchmark-datasets/dna/easy/wickd3b_7705.processed.fasta");
+    criterion.bench_function("DNA easy small", |bench| {
+        bench.iter_batched(
             // clone because of interior mutability
-            c.clone().cost();
-        });
+            || c.clone(),
+            |cost_fn| cost_fn.cost(),
+            criterion::BatchSize::SmallInput,
+        );
+    });
+}
+
+fn pip_cost_dna_easy_medium(criterion: &mut Criterion) {
+    let c = setup::<JC69>(
+        "data/benchmark-datasets/dna/easy/tarvd7_ENSG00000126777.nuc.ord.processed.fasta",
+    );
+    criterion.bench_function("DNA easy medium", |bench| {
+        bench.iter_batched(
+            // clone because of interior mutability
+            || c.clone(),
+            |cost_fn| cost_fn.cost(),
+            criterion::BatchSize::SmallInput,
+        );
+    });
+}
+
+fn pip_cost_dna_easy_large(criterion: &mut Criterion) {
+    let c = setup::<JC69>("data/benchmark-datasets/dna/easy/misod2a_EOG5CVDNS.processed.fasta");
+    criterion.bench_function("DNA easy large", |bench| {
+        bench.iter_batched(
+            // clone because of interior mutability
+            || c.clone(),
+            |cost_fn| cost_fn.cost(),
+            criterion::BatchSize::SmallInput,
+        );
     });
 }
 
 criterion_group! {
-name = pip_cost;
+name = pip_cost_easy;
 config = Criterion::default();
-targets = pip_cost_tiny_but_long
+targets = pip_cost_dna_easy_small, pip_cost_dna_easy_medium, pip_cost_dna_easy_large,
 }
-criterion_main!(pip_cost);
+criterion_main!(pip_cost_easy);
