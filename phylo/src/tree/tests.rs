628e7b27cfee9421a9079624ab1e095aae516d00
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-04-17 21:57:42 +0200

    Adjust NJ tests to correct for deterministic runs

--- phylo/src/tree/tests.rs
+++ phylo/src/tree/tests.rs
@@ -179,12 +179,12 @@ fn nj_correct() {
     let nodes = vec![
         Node::new_leaf(0, Some(I(5)), 2.0, "A0".to_string()),
         Node::new_leaf(1, Some(I(5)), 3.0, "B1".to_string()),
-        Node::new_leaf(2, Some(I(6)), 4.0, "C2".to_string()),
-        Node::new_leaf(3, Some(I(7)), 2.0, "D3".to_string()),
-        Node::new_leaf(4, Some(I(7)), 1.0, "E4".to_string()),
-        Node::new_internal(5, Some(I(6)), vec![L(1), L(0)], 3.0, "".to_string()),
-        Node::new_internal(6, Some(I(8)), vec![L(2), I(5)], 1.0, "".to_string()),
-        Node::new_internal(7, Some(I(8)), vec![L(4), L(3)], 1.0, "".to_string()),
+        Node::new_leaf(2, Some(I(7)), 4.0, "C2".to_string()),
+        Node::new_leaf(3, Some(I(6)), 2.0, "D3".to_string()),
+        Node::new_leaf(4, Some(I(6)), 1.0, "E4".to_string()),
+        Node::new_internal(5, Some(I(7)), vec![L(1), L(0)], 3.0, "".to_string()),
+        Node::new_internal(6, Some(I(8)), vec![L(4), L(3)], 1.0, "".to_string()),
+        Node::new_internal(7, Some(I(8)), vec![I(5), L(2)], 1.0, "".to_string()),
         Node::new_internal(8, None, vec![I(7), I(6)], 0.0, "".to_string()),
     ];
     assert_eq!(nj_tree.root, I(8));
@@ -276,10 +276,10 @@ fn nj_correct_wiki_example() {
     assert_eq!(tree.by_id("a").blen, 2.0);
     assert_eq!(tree.by_id("b").blen, 3.0);
     assert_eq!(tree.by_id("c").blen, 4.0);
-    assert_eq!(tree.by_id("d").blen, 1.0);
+    assert_eq!(tree.by_id("d").blen, 2.0);
     assert_eq!(tree.by_id("e").blen, 1.0);
     assert_eq!(tree.node(&I(5)).blen, 3.0);
-    assert_eq!(tree.node(&I(6)).blen, 2.0);
+    assert_eq!(tree.node(&I(6)).blen, 1.0);
     assert_eq!(tree.node(&I(7)).blen, 1.0);
     assert_eq!(tree.len(), 9);
     assert_eq!(tree.postorder.len(), 9);
