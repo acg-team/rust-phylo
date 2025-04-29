f7665a6cb07d525fa884b1be4c734692a504a614
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-04-29 19:13:26 +0200

    Remove old todo comment

--- phylo/src/optimisers/topo_optimiser.rs
+++ phylo/src/optimisers/topo_optimiser.rs
@@ -14,7 +14,6 @@ pub struct TopologyOptimiser<C: TreeSearchCost + Display + Clone> {
 }
 
 impl<C: TreeSearchCost + Clone + Display> TopologyOptimiser<C> {
-    // TODO: make tree search work under parsimony
     pub fn new(cost: C) -> Self {
         Self {
             epsilon: 1e-3,
