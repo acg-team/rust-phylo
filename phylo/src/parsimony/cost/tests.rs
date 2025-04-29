2179f519247f2d6280402eef086429af920546db
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-04-29 16:34:40 +0200

    Add reroot test for BasicParsimonyCost

--- phylo/src/parsimony/cost/tests.rs
+++ phylo/src/parsimony/cost/tests.rs
@@ -35,7 +35,35 @@ fn basic_parsimony_cost() {
         tree: tree3,
     };
     let cost = BasicParsimonyCost::new(info).unwrap();
-    assert_eq!(cost.cost(), 6.0);
+    assert_eq!(cost.cost(), -6.0);
+}
+
+#[test]
+fn basic_parsimony_reroot() {
+    let seqs = Sequences::new(vec![
+        record!("A", b"GGA"),
+        record!("B", b"GGG"),
+        record!("C", b"ACA"),
+        record!("D", b"ACG"),
+    ]);
+    let tree = tree!("((A:1.0,B:1.0):1.0,(C:1.0,D:1.0):1.0):0.0;");
+
+    let info = PhyloInfo {
+        msa: Alignment::from_aligned(seqs.clone(), &tree).unwrap(),
+        tree,
+    };
+    let cost = BasicParsimonyCost::new(info).unwrap();
+    assert_eq!(cost.cost(), -4.0);
+
+    let tree_reroot = tree!("(((D:1,(B:1,A:0.5):0.5):1,C:2):0);");
+
+    let info_reroot = PhyloInfo {
+        msa: Alignment::from_aligned(seqs.clone(), &tree_reroot).unwrap(),
+        tree: tree_reroot,
+    };
+    let cost_reroot = BasicParsimonyCost::new(info_reroot).unwrap();
+    assert_eq!(cost_reroot.cost(), -4.0);
+    assert_eq!(cost_reroot.cost(), cost.cost());
 }
 
 #[test]
