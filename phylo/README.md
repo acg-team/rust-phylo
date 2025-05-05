be551d37e7431a6542232baa92b29db9a9cf3c74
Merlin <merlinio2000@users.noreply.github.com>
2025-05-05 14:49:32 +0200

    Feature/benches (#45)
    * Add randomisation to tree search
    
    Added randomisation to the tree search that only kicks in when the deterministic
    feature is not switched on
    
    * Rename misleading true_tree -> example_tree
    
    * Remove accidentally added file
    
    * avoid unnecessary prune location modifications
    
    * avoid accidental misuse by feature gating rng function
    
    * add initial benchmarks with criterion
    
    * add benchmarks for isolated call of cost
    
    * use pip instead uf subst for cost bench
    
    * start dumping some notes
    
    * parallelize accross regraft_locations in topology optimiser
    
    * Revert "parallelize accross regraft_locations in topology optimiser" (see experiment/parallel_topo)
    
    This reverts commit c80e730191a58695750287fb8468b1be0f71e0f0.
    
    * remove relicts
    
    * change default measure time and size
    
    * add some simple compiler settings to play with
    
    * add benchmark datasets + readme with cc attribution
    
    * adapt cost benchmark to new datasets
    
    * cost benchmark: add some aa datasets
    
    * make benchmark names more descriptive | standardize bench handling
    
    * try to paint a more insightful picture in tree_from_msa
    
    * minor cleanup
    
    * make bench names more explicit to avoid conflicts
    
    * add topology optimiser benches
    
    * add cost benches for some LARGE alignments
    
    * fix: use deterministic NJ trees as a starting point
    
    * increase running time of benches for more stability
    
    * adapt to new 'deterministic' feature flag
    
    * refactor topo_optimiser for easier benching
    
    * more topo_optimser refactor for benching
    
    * run topo benches for a fixed number of iterations to prevent inconsistencies
    
    * remove now unnecessary workarounds for benchmarking
    
    * split spr and topo benches
    
    * choose more reasonable worse-case prune locations for isolated benches
    
    * comment fix
    
    * refactor: regraft cost tuple to struct because of confusion potential
    
    * refactor bench setup to avoid exposing helpers to library consumers
    
    * fix cargo trying to bench helpers
    
    * add flamegraph as profiler to all benchmarks
    
    * use default frequency from flamegraph repo for bench flamegraphs
    
    * remove commented out bench sets
    
    * cleanup Cargo.toml
    
    * move find_possible_prune_locations into Tree
    
    * refactor topo optimiser to make public interface more natural
    
    * fix path/branch terminology in comment
    
    * unify bench setup functions and move to helper
    
    * fix typo in bench name
    
    * remove topo duplication in bench by making topo_opt predicate configurable
    
    * fix comment
    
    ---------
    
    Co-authored-by: Jūlija Pečerska <julija.pecerska@zhaw.ch>
    Co-authored-by: Merlin Maggi <merlin.maggi@jls.ch>

---
+++ phylo/README.md
@@ -0,0 +1,7 @@
+# Attributions
+
+Datasets for benchmarking were taken from (licensed under [CC BY 4.0]( https://creativecommons.org/licenses/by/4.0/ ))
+The datasets were modified by normalizing invalid/unrecognized sequence characters since the exact sequences are less relevant
+for pure performance measurements.
+- Zhou, Xiaofan (2017). Single-gene alignments. figshare. Dataset. https://doi.org/10.6084/m9.figshare.5477749.v1
+- Zhou, Xiaofan (2017). Supermatrices. figshare. Dataset. https://doi.org/10.6084/m9.figshare.5477746.v1
---
+++ phylo/benches/cost.rs
@@ -0,0 +1,72 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use phylo::{
+    evolutionary_models::FrequencyOptimisation,
+    likelihood::ModelSearchCost,
+    pip_model::PIPCost,
+    substitution_models::{QMatrix, QMatrixMaker, JC69, WAG},
+};
+mod helpers;
+use helpers::{
+    black_box_pip_cost, SequencePaths, AA_EASY_12X73, AA_EASY_14X165, AA_EASY_27X632,
+    AA_EASY_45X223, AA_EASY_6X97, AA_MEDIUM_79X106, DNA_EASY_17X2292, DNA_EASY_33X4455,
+    DNA_EASY_46X16250, DNA_EASY_5X1000, DNA_EASY_8X1252, DNA_MEDIUM_128X688,
+};
+
+fn run_for_sizes<Q: QMatrix + QMatrixMaker>(
+    paths: &SequencePaths,
+    group_name: &'static str,
+    criterion: &mut Criterion,
+) {
+    let mut bench_group = criterion.benchmark_group(group_name);
+    let mut bench = |id: &str, data: PIPCost<Q>| {
+        bench_group.bench_function(id, |bench| {
+            bench.iter_batched(
+                // clone because of interior mutability
+                || data.clone(),
+                |cost_fn| cost_fn.cost(),
+                criterion::BatchSize::SmallInput,
+            );
+        });
+    };
+    for (key, path) in paths {
+        let data = black_box_pip_cost::<Q>(path, FrequencyOptimisation::Empirical);
+        bench(key, data);
+    }
+    bench_group.finish();
+}
+
+fn pip_cost_dna_easy(criterion: &mut Criterion) {
+    let paths = SequencePaths::from([
+        ("5X1000", DNA_EASY_5X1000),
+        ("8X1252", DNA_EASY_8X1252),
+        ("17X2292", DNA_EASY_17X2292),
+        ("33X4455", DNA_EASY_33X4455),
+        ("46X16250", DNA_EASY_46X16250),
+        ("128X688", DNA_MEDIUM_128X688),
+    ]);
+    run_for_sizes::<JC69>(&paths, "PIP Cost DNA", criterion);
+}
+
+fn pip_cost_aa_easy(criterion: &mut Criterion) {
+    let paths = SequencePaths::from([
+        ("6X97", AA_EASY_6X97),
+        ("12X73", AA_EASY_12X73),
+        ("14X165", AA_EASY_14X165),
+        ("27X632", AA_EASY_27X632),
+        ("45X223", AA_EASY_45X223),
+        ("79X106", AA_MEDIUM_79X106),
+    ]);
+    run_for_sizes::<WAG>(&paths, "PIP Cost AA", criterion);
+}
+
+criterion_group! {
+name = pip_cost_dna;
+config = helpers::setup_suite();
+targets = pip_cost_dna_easy,
+}
+criterion_group! {
+name = pip_cost_aa;
+config = helpers::setup_suite();
+targets = pip_cost_aa_easy,
+}
+criterion_main!(pip_cost_dna, pip_cost_aa);
