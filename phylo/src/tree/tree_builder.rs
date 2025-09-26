7361e0155f0c334713814ab738fa34143ad997dc
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-09-26 20:11:42 +0200

    Add doctring to TreeBuilder trait

--- phylo/src/tree/tree_builder.rs
+++ phylo/src/tree/tree_builder.rs
@@ -2,6 +2,7 @@ use crate::alignment::Sequences;
 use crate::tree::Tree;
 use crate::Result;
 
+/// A trait for building phylogenetic trees from a set of sequences.
 pub trait TreeBuilder {
     fn build(&self, seqs: &Sequences) -> Result<Tree>;
 }
