71c2d141ac511d35c712911214b970c612ab3ed4
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-09-30 16:45:15 +0200

    Clean up LevenshteinDNACorrected distance
    Clean up LevenshteinDNACorrected distance, add derives and clean up comments

--- phylo/src/evolutionary_distances/mod.rs
+++ phylo/src/evolutionary_distances/mod.rs
@@ -5,20 +5,23 @@ pub trait EvolutionaryDistance {
     fn dist(&self, a: &Record, b: &Record) -> f64;
 }
 
-#[derive(Default)]
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq, PartialOrd, Ord)]
+/// Levenshtein distance with Jukes-Cantor correction, meaningful for DNA sequences
 pub struct LevenshteinDNACorrected;
 
-// Distance function to be used for default NJ Builder impl
 impl EvolutionaryDistance for LevenshteinDNACorrected {
     fn dist(&self, a: &Record, b: &Record) -> f64 {
+        // Distance formula corrected using the Jukes-Cantor model
         let seq_i = a.seq();
         let seq_j = b.seq();
+        // To avoid infinite distance when all characters are different, the maximum
+        // proportion of different characters is capped to 3/4=0.75
+        let max_proportion = 3.0 / 4.0;
         let dist = levenshtein(seq_i, seq_j) as f64;
         let proportion_diff = f64::min(
             dist / (max(seq_i.len(), seq_j.len()) as f64),
-            0.75 - f64::EPSILON,
+            max_proportion - f64::EPSILON,
         );
-        // TODO: Doesn't work for proteins! @junniest
-        -3.0 / 4.0 * (1.0 - 4.0 / 3.0 * proportion_diff).ln()
+        -max_proportion * (1.0 - 1.0 / max_proportion * proportion_diff).ln()
     }
 }
