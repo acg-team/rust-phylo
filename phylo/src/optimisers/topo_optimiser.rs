bb6a137141cf8565758059fed053b7cb56c6ee72
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-10-06 11:44:35 +0200

    Remove unnecessary parameter in optimise_iteration
    Remove unnecessary parameter from TopologyOptimiser::optimise_iteration, it used to take
    the current cost, now it's computed/read from the cost function

--- phylo/src/optimisers/topo_optimiser.rs
+++ phylo/src/optimisers/topo_optimiser.rs
@@ -116,7 +116,7 @@ where
             iterations += 1;
             info!("Iteration: {iterations}, current cost: {curr_cost}");
             prev_cost = curr_cost;
-            curr_cost = self.optimise_iteration(curr_cost)?;
+            curr_cost = self.optimise_iteration()?;
             delta = curr_cost - prev_cost;
             costs.push(curr_cost);
         }
@@ -133,9 +133,10 @@ where
         })
     }
 
-    /// Performs a single iteration of branch length optimisation over all branches in the tree.
-    /// Returns the cost after optimising all branches once.
-    fn optimise_iteration(&mut self, init_cost: f64) -> Result<f64> {
+    /// Performs a single iteration of topology optimisation over all nodes/branches in the tree.
+    /// Returns the cost after going through all possible moves once.
+    fn optimise_iteration(&mut self) -> Result<f64> {
+        let init_cost = self.c.cost();
         let possible_move_locs: Vec<_> = self.move_opti.move_locations(&self.c).copied().collect();
         let mut current_move_locs: Vec<_> = possible_move_locs.iter().collect();
 
