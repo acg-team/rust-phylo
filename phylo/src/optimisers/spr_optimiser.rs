0d40603bc4af1089205e145c50767bef87a87f66
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2025-10-09 22:43:28 +0200

    Remove max brent iterations from spr optimiser
    Remove max brent iterations from spr optimiser because it was causing the debug_assert!
    in topo_optimiser::run() to fail. Still no idea why that is the case.

--- phylo/src/optimisers/spr_optimiser.rs
+++ phylo/src/optimisers/spr_optimiser.rs
@@ -1,5 +1,4 @@
 use std::fmt::Display;
-use std::num::NonZeroU64;
 
 use anyhow::bail;
 use approx::relative_eq;
@@ -8,7 +7,7 @@ use log::{debug, info};
 
 use crate::likelihood::TreeSearchCost;
 use crate::optimisers::move_optimiser::{MoveCostInfo, MoveOptimiser};
-use crate::optimisers::optimise_branch_w_iters;
+use crate::optimisers::optimise_branch;
 use crate::tree::{NodeIdx, Tree};
 use crate::Result;
 
@@ -221,8 +220,7 @@ fn calc_spr_cost_with_blen_opt<C: TreeSearchCost + Clone + Display>(
     let mut move_cost = cost_fn.cost();
     if cost_fn.blen_optimisation() && move_cost <= base_cost {
         // reoptimise branch length at the regraft location
-        let blen_opt =
-            optimise_branch_w_iters(&cost_fn, &regraft, Some(NonZeroU64::new(5).unwrap()))?;
+        let blen_opt = optimise_branch(&cost_fn, &regraft)?;
         if blen_opt.final_cost > move_cost {
             move_cost = blen_opt.final_cost;
             new_tree.set_blen(&regraft, blen_opt.value);
